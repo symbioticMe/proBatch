70e141b39d0c6a238bbcf6f0fd502d5c2c2b47c5
Jelena Chuklina <chuklina.jelena@gmail.com>
2018-03-21 16:06:04 +0100

    initialize the package and its basics: normalization and some auxiliary scripts

---
+++ R/aux.R
@@ -0,0 +1,140 @@
+library(WGCNA)
+
+#' Title
+#'
+#' @param df_long
+#' @param sample_annotation
+#' @param batch_column
+#' @param feature_id_column
+#' @param threshold_batch
+#' @param threshold_global
+#'
+#' @return
+#' @export
+#' @import tidyverse
+#'
+#' @examples
+clean_requants <- function(df_long, sample_annotation, batch_column,
+                           feature_id_column = 'peptide_group_label',
+                           threshold_batch = .7, threshold_global = .5){
+  #for dplyr version 0.7 and higher, this is the way to call the functions
+  feature_id_column_var <- quo(feature_id_column)
+
+  peptide_summary = df_long %>%
+    merge(sample_annotation) %>%
+    dplyr::select(one_of(c(feature_id_column, batch_column, 'm_score'))) %>%
+    group_by(!!(c(feature_id_column, batch_column))) %>%
+    mutate(n_requants_batch = sum(m_score == 2), n_batch = n()) %>%
+    mutate(requant_fraction_batch = n_requants_batch/n_batch) %>%
+    group_by_at(vars(one_of(c(feature_id_column)))) %>%
+    mutate(n_requants = sum(m_score == 2), n = n()) %>%
+    mutate(requant_fraction = n_requant/n) %>%
+    filter(requant_fraction < threshold_global) %>%
+    filter(requant_fraction_batch < threshold_batch)
+  return(df_long %>% filter(!!feature_id_column %in% peptide_summary[[feature_id_column]]))
+}
+
+sample_annotation_to_colors <- function(sample_annotation, columns_to_include = NULL,
+                                        columns_to_exclude = NULL){
+
+}
+
+#' Title
+#'
+#' @param proteome
+#'
+#' @return
+#' @export
+#' @import tidyverse
+#'
+#' @examples
+summarize_peptides <- function(proteome){
+  peptide_summary = proteome %>%
+    group_by(FullRunName) %>%
+    mutate(rank = rank(Intensity))  %>%
+    group_by(peptide_group_label, ProteinName) %>%
+    summarise(RT_mean = mean(RT),
+              Int_mean = mean(Intensity), rank_mean = mean(rank),
+              numb_requants = sum(m_score > 1),
+              mean_m_score = mean(m_score),
+              median_m_score = median(m_score),
+              median_good_m_score = median(m_score[m_score < 1]))
+}
+
+#' Convert from long data frame to data matrix (features in rows, samples in columns)
+#'
+#' @param proteome_long
+#' @param feature_id_column
+#' @param measure_column
+#' @param sample_id_column
+#'
+#' @return
+#' @export
+#' @import tidyverse
+#'
+#' @examples
+convert_to_matrix <- function(data_df_long,
+                              feature_id_column = 'peptide_group_label',
+                              measure_column = 'Intensity',
+                              sample_id_column = 'FullRunName'){
+  casting_formula =  as.formula(paste(feature_id_column, sample_id_column,
+                                      sep =  " ~ "))
+  proteome_wide = dcast(data_df_long, formula=casting_formula,
+                        value.var=measure_column) %>%
+    column_to_rownames(feature_id_column) %>%
+    as.matrix()
+  return(proteome_wide)
+}
+
+#' Convert the features x samples data matrix to a long format (e.g. for plotting)
+#'
+#' @param data_matrix
+#' @param step
+#' @param sample_annotation
+#'
+#' @return
+#' @export
+#' @import tidyverse, reshape2
+#'
+#' @examples
+matrix_to_long <- function(data_matrix, sample_annotation, measure_col = 'Intensity', step){
+  df_long = data_matrix %>%
+    as.data.frame() %>%
+    rownames_to_column(var = 'peptide_group_label') %>%
+    melt(id.var = 'peptide_group_label', value.name = measure_col,
+         variable.name = 'FullRunName') %>%
+    mutate(before_after = step) %>%
+    merge(sample_annotation)
+  return(df_long)
+}
+
+#' Add order and dateTime column to sample annotation data frame
+#'
+#' @param sample_annotation
+#' @param time_column
+#' @param dateTimeFormat
+#' @param order_column
+#'
+#' @return
+#' @export
+#' @import tidyverse
+#'
+#' @examples
+date_to_order <- function(sample_annotation, time_column,
+                          dateTimeFormat = c("%b_%d", "%H:%M:%S"),
+                          order_column = 'order'){
+  if (length(time_column) == 1){
+    sample_annotation$run_order = as.POSIXct(sample_annotation[[time_column]],
+                                             format=dateTimeFormat)
+  }
+  else {
+    sample_annotation = sample_annotation %>%
+      mutate(dateTime = as.POSIXct(paste(), paste(dateTimeFormat)))
+  }
+
+  #sample_annotation$run_order = as.POSIXct(paste(sample_annotation$RunDate,
+  #                                               sample_annotation$RunTime),
+  #                                         format=dateTimeFormat)
+  sample_annotation[[order_column]] = rank(sample_annotation$run_order)
+  return(sample_annotation)
+}
---
+++ R/normalize.R
@@ -0,0 +1,118 @@
+library(lazyeval)
+
+
+#' Quantile normalization of the data, ensuring that the row and column names are retained
+#'
+#' @param data_matrix log transformed data matrix (features in rows and samples in columns)
+#'
+#' @return
+#' @export
+#' @import preprocessCore
+#'
+#' @examples
+quantile_normalize <- function(data_matrix){
+  q_norm_proteome = normalize.quantiles(data_matrix)
+  colnames(q_norm_proteome) = colnames(data_matrix)
+  rownames(q_norm_proteome) = rownames(data_matrix)
+  return(q_norm_proteome)
+}
+
+#' Median normalization of the data
+#'
+#' @param sample_annotation
+#' @param batch_column
+#' @param measure_column
+#' @param data_matrix
+#'
+#' @return
+#' @export
+#' @import tidyverse, lazyeval
+#'
+#' @examples
+median_normalization <- function(data_matrix, sample_annotation,
+                                 batch_column, measure_column)
+  {
+  data_matrix = data_matrix %>% merge(sample_annotation) %>%
+    group_by_at(vars(one_of(batch_column))) %>%
+    mutate_(median = interp(~median(measurement),
+            measurement = as.name(measure_column))) %>%
+    ungroup() %>%
+    mutate_(median_overall = interp(~median(measurement),
+            measurement = as.name(measure_column))) %>%
+    mutate(diff = median - median_overall) %>%
+    mutate()
+}
+
+#' normalize with the custom (continuous) fit
+#'
+#' @param data_matrix
+#' @param sample_annotation
+#' @param batch_col
+#' @param feature_id_col
+#' @param sample_id_column
+#' @param measure_col
+#' @param sample_order_col
+#' @param fit_func
+#' @param return_long
+#' @param ... other parameters, usually those of the `fit_func`
+#'
+#' @return
+#' @export
+#' @import tidyverse, reshape2, lazyeval
+#'
+#' @examples
+normalize_custom_fit <- function(data_matrix, sample_annotation, batch_col,
+                                 feature_id_col, sample_id_column, measure_col,
+                                 sample_order_col, fit_func, return_long = F, ...){
+  data_matrix = as.data.frame(data_matrix)
+  data_matrix[[feature_id_col]] = rownames(data_matrix)
+  df_long = data_matrix %>%
+    melt(id.vars = feature_id_col)
+  names(df_long) = c(feature_id_col, sample_id_column, measure_col)
+
+  df_normalized = df_long %>%
+    merge(sample_annotation) %>%
+    arrange_(feature_id_col, sample_order_col) %>%
+    group_by_at(vars(one_of(c(feature_id_col, batch_col)))) %>%
+    nest() %>%
+    mutate(fit = map(data, fit_func, response.var = measure_col,
+                     expl.var = sample_order_col, ...)) %>%
+    unnest() %>%
+    #change the fit to the corrected data
+    group_by_at(vars(one_of(c(feature_id_col, batch_col)))) %>%
+    mutate(mean_fit = mean(fit)) %>%
+    mutate(diff = mean_fit - fit) %>%
+    mutate_(Intensity_normalized = interp(~`+`(x, y),
+                                          x = as.name('diff'),
+                                          y = as.name(measure_col)))
+    #if only the fitted data table is required (not recommended)
+    if(!return_long){
+      casting_formula =  as.formula(paste(feature_id_col, sample_id_column,
+                                          sep =  " ~ "))
+      df_normalized = dcast(df_normalized, formula=casting_formula,
+                            value.var = 'Intensity_normalized')
+                     }
+  return(df_normalized)
+}
+
+
+#' Standardized input-output ComBat normalization
+#'
+#' @param sample_annotation
+#' @param batch_column
+#' @param par.prior
+#' @param data_matrix
+#'
+#' @return
+#' @export
+#' @import sva
+#'
+#' @examples
+correct_with_ComBat <- function(data_matrix, sample_annotation,
+                                batch_column = 'MS_batch.final', par.prior = TRUE){
+  batches = sample_annotation[[batch_column]]
+  modCombat = model.matrix(~1, data = sample_annotation)
+  corrected_proteome = ComBat(dat = data_matrix, batch = batches,
+                              mod = modCombat, par.prior = par.prior)
+  return(corrected_proteome)
+}
