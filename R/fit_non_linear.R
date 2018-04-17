8cd6cc29f81003cc3928a6799f86ad4fd797ca49
Jelena Chuklina <chuklina.jelena@gmail.com>
2018-04-17 15:39:45 +0200

    created the documentation for non-linear fit function
    Former-commit-id: 62e0bedfcb6d64c1d852d51c95dcf3575a2afd5c [formerly 65ccdc79f0b6df1a045d22684c8e267ec53b9d06]
    Former-commit-id: 24900f2c8ec04219c58e39abc75288607ce52cb6

--- R/fit_non_linear.R
+++ R/fit_non_linear.R
@@ -1,5 +1,19 @@
+#' Fit a non-linear trend
+#'
+#' @param dataDF
+#' @param response.var
+#' @param expl.var
+#' @param noFitRequants
+#' @param fitFunc
+#' @param with_df
+#' @param ...
+#'
+#' @return
+#' @export
+#'
+#' @examples
 fit_nonlinear <- function(dataDF, response.var = 'y', expl.var = 'x',
-                          noFitRequants = F, fitFunc = 'kernel_smooth', 
+                          noFitRequants = F, fitFunc = 'kernel_smooth',
                           with_df = F, ...){
   dataDF <- dataDF[sort.list(dataDF[[expl.var]]),]
   x_to_fit = dataDF[[expl.var]]
@@ -9,7 +23,7 @@ fit_nonlinear <- function(dataDF, response.var = 'y', expl.var = 'x',
     x_all[dataDF$requant] = NA
   }
   if(!with_df){
-    fit_res = switch(fitFunc, 
+    fit_res = switch(fitFunc,
            kernel_smooth = kernel_smooth(x_all, y, x_to_fit, ...),
            smooth_spline = smooth_spline(x_all, y, x_to_fit, ...),
            loess_regression = loess_regression(x_all, y, x_to_fit, ...)
@@ -26,28 +40,28 @@ fit_nonlinear <- function(dataDF, response.var = 'y', expl.var = 'x',
 }
 
 
-loocv.nw <- function(reg.data, bw = 1.5, kernel = "normal", 
+loocv.nw <- function(reg.data, bw = 1.5, kernel = "normal",
                      response.var = 'y', expl.var = 'x', ...){
   ## Help function to calculate leave-one-out regression values
   loo.reg.value.bw <- function(i, reg.data, bw, kernel = kernel,
                                response.var = 'y', expl.var = 'x')
-    return(ksmooth(reg.data[[expl.var]][-i], reg.data[[response.var]][-i], 
-                   x.points = reg.data[[expl.var]][i], 
+    return(ksmooth(reg.data[[expl.var]][-i], reg.data[[response.var]][-i],
+                   x.points = reg.data[[expl.var]][i],
                    kernel = kernel, bandwidth = bw)$y)
-  
+
   ## Calculate LOO regression values using the help function above
   n <- nrow(reg.data)
   loo.values.bw <- sapply(1:n, loo.reg.value.bw, reg.data, bw, kernel,
                           response.var, expl.var)
-  
+
   ## Calculate and return MSE
   return(mean((reg.data[[response.var]] - loo.values.bw)^2))
 }
 
-optimise_bw <- function(dataDF, response.var = 'y', expl.var = 'x', 
+optimise_bw <- function(dataDF, response.var = 'y', expl.var = 'x',
                         bws = c(0.01, 0.5, 1, 1.5, 2, 5, 10)){
-  cv.nw_mult = sapply(bws, function(bw) loocv.nw(dataDF, bw, 
-                                                 response.var = response.var, 
+  cv.nw_mult = sapply(bws, function(bw) loocv.nw(dataDF, bw,
+                                                 response.var = response.var,
                                                  expl.var = expl.var))
   bw_best = bws[which.min(cv.nw_mult)]
   return(bw_best)
@@ -58,7 +72,7 @@ reg.fcn.nw <- function(reg.x, reg.y, x, bw = 1.5)
 
 optimise_df <- function(dataDF, bw, response.var = 'y', expl.var = 'x'){
   #find the best bandwidth
-  
+
   n <- nrow(dataDF)
   Id <- diag(n)
   S.nw <- matrix(0, n, n)
@@ -69,14 +83,14 @@ optimise_df <- function(dataDF, bw, response.var = 'y', expl.var = 'x'){
 }
 
 kernel_smooth <- function(x_all, y, x_to_fit, bw = 1.5, ...){
-  res = ksmooth(x_to_fit, y, x.point = x_all, 
+  res = ksmooth(x_to_fit, y, x.point = x_all,
           kernel = "normal", bandwidth = bw)$y
   return(res)
 }
 
 smooth_spline <- function(x_all, y, x_to_fit, ...){
   #allow to define nknots among other parameters or spar or whatever
-  
+
   nknots = length(x_all)/2
 
   fit = smooth.spline(x_to_fit, y, nknots = nknots)
@@ -93,7 +107,7 @@ loess_regression <- function(x_all, y, x_to_fit, ...){
 kernel_smooth_opt <- function(x_all, y, x_to_fit, bw, kernel = "normal", ...){
   temp_x_all = x_all[!is.na(x_all)]
   y_for_model = y[!is.na(x_all)]
-  res = ksmooth(temp_x_all, y_for_model, x.points = x_to_fit, 
+  res = ksmooth(temp_x_all, y_for_model, x.points = x_to_fit,
                 kernel = kernel, bandwidth = bw)$y
   return(res)
 }
