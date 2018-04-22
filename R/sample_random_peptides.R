0ff1a43e62cafebc36c1f9ed85de5fa83eff2ab5
Jelena Chuklina <chuklina.jelena@gmail.com>
2018-04-22 20:01:19 +0200

    shipping package version 1.0.
    Former-commit-id: 0786c5c2eee6dcd26dc38551984e6f1f2ca50307 [formerly e375911abbce992c7f1b162141d464a29d64d507]
    Former-commit-id: 1d03d5a9c1417a0a6f50888fd6adeb2a27572ffe

---
+++ R/sample_random_peptides.R
@@ -0,0 +1,47 @@
+cut_into_groups <- function(vect, groups, quantile = T){
+  if (quantile){
+    cutting_points = unique(quantile(vect, probs = seq(0, 1, length = groups + 1)))
+  } else {
+    #equally distance RT bins 10 (into number of groups - as argument of the function)
+    cut_step = (max(vect)- min(vect))/(groups)
+    cutting_points = min(vect)+0:(groups)*cut_step
+  }
+  res_groups = cut(vect, cutting_points, include.lowest = TRUE)
+  return(res_groups)
+}
+
+#' sample random peptides for diagnostics
+#'
+#' @param summarized_proteome
+#'
+#' @return
+#' @export
+#' @importFrom magrittr %>%
+#' @import dplyr
+#'
+#' @examples
+sample_random_peptides <- function(proteome, seed = 1, pep_per_group = 3,
+                                   groups_RT = 10, groups_intensity = 5){
+  summarized_proteome = proteome %>%
+    filter(m_score < 1) %>%
+    mutate(log_intensity = log2(Intensity + 1)) %>%
+    group_by(peptide_group_label) %>%
+    summarise(RT_average = mean(RT),
+              Intensity_average = mean(log_intensity))
+  summarized_proteome = summarized_proteome %>%
+    mutate(RT_quantile = cut_into_groups(RT_average, groups_RT),
+           Int_quantile = cut_into_groups(Intensity_average, groups_intensity)) %>%
+    group_by(RT_quantile, Int_quantile)
+
+  set.seed(seed)
+  sampled_peptide_df = summarized_proteome %>%
+    #sample 3 peptides per each group
+    sample_n(pep_per_group) %>%
+    arrange(RT_average)
+  sampled_peptide_df1 = sampled_peptide_df %>%
+    group_by(RT_quantile, Int_quantile) %>% arrange(RT_average) %>%
+    mutate(group = rank(peptide_group_label))
+  peptides_to_plot = sampled_peptide_df$peptide_group_label
+  return(list(peptide_summary_df = summarized_proteome,
+         peptides = peptides_to_plot))
+}
