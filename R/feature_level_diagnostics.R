fadbc17f2c69bc8f69bd1f7bbe54a10889fb5f03
Jelena Chuklina <chuklina.jelena@gmail.com>
2018-05-06 19:45:21 +0200

    cleaned argument names + wrote the documentation
    Former-commit-id: b782076fffd12019c1cd3da804d3d3b1f133ebd9
    Former-commit-id: b9884784a9029a5d483235fa234be56254faa5e4

--- R/feature_level_diagnostics.R
+++ R/feature_level_diagnostics.R
@@ -1,33 +1,49 @@
-#' plot a single peptide or several peptides each in its own facet
+#' Plot a single feature or several features each in its own facet
 #'
-#' @param pep_name
-#' @param data_df_long
-#' @param sample_annotation
-#' @param order_column
-#' @param measurement.col
-#' @param batch_column
-#' @param feature_id_column
-#' @param geom
-#' @param facet_by_batch
-#' @param title
-#' @param requant
-#' @param sample_id_column
-#' @param color_by_batch
-#' @param theme
+#' single feature can also be spike-ins or iRT peptides (in DIA proteomics)
 #'
-#' @return
+#' @param pep_name name of the peptide for diagnostic profiling
+#' @param df_long data frame where each row is a single feature in a single sample,
+#' thus it has minimally, `sample_id_col`, `feature_id_column` and `measure_column`,
+#' but usually also `m_score` (in OpenSWATH output result file)
+#' @param sample_annotation data matrix with 1) `sample_id_col` (this can be repeated as row names)
+#' 2) biological and 3) technical covariates (batches etc)
+#' @param order_column sample injection order
+#' @param sample_id_col name of the column in sample_annotation file,
+#' where the filenames (colnames of the data matrix are found)
+#' @param batch_column column in `sample_annotation` that should be used for batch comparison
+#' @param measure_column if `df_long` is among the parameters, it is the column with
+#' expression/abundance/intensity, otherwise, it is used internally for consistency
+#' @param feature_id_column name of the column with feature/gene/peptide/protein ID
+#' used with long format matrix (`df_long`).
+#' Identical to wide format (`data_matrix`) to row name
+#' @name feature_level_diagnostics
+
+
+#' @name feature_level_diagnostics
+#' @param geom whether to show the feature as points and/or connect by lines
+#' @param color_by_batch (logical) whether to color points by batch
+#' @param facet_by_batch (logical) whether to plot each batch in its own facet
+#' @param title the string indicating the source of the peptides
+#' @param requant if data frame: requant values;
+#' if logical: whether to indicate requant values (requires 'requant' or 'm_score' column in `df_long`)
+#' @param theme plot theme (default is 'classical'; other options not implemented)
+#'
+#' @return ggplot2 type plot of `measure_column` vs `order`,
+#' faceted by peptide name and (optionally) by batch
 #' @export
 #' @import ggplot2
 #' @import dplyr
 #' @importFrom magrittr %>%
 #'
 #' @examples
-plot_peptide_level <- function(pep_name, data_df_long, sample_annotation,
+plot_peptide_level <- function(pep_name, df_long, sample_annotation,
+                               order_column = NULL,
+                               sample_id_col = 'FullRunName',
                                batch_column = 'MS_batch.final',
+                               measure_column = 'Intensity',
                                feature_id_column = 'peptide_group_label',
-                               measurement.col = 'Intensity',
-                               sample_id_column = 'FullRunName',
-                               order_column = NULL, geom = c('point', 'line'),
+                               geom = c('point', 'line'),
                                color_by_batch = F, facet_by_batch = F,
                                title = NULL, requant = NULL, theme = 'classic'){
   #TODO: suggest faceting by instrument
@@ -35,17 +51,17 @@ plot_peptide_level <- function(pep_name, data_df_long, sample_annotation,
 
 
 
-  plot_df = data_df_long %>%
+  plot_df = df_long %>%
     filter(rlang::UQ(as.name(feature_id_column)) %in% pep_name)
-  if (!all(names(sample_annotation) %in% names(data_df_long))){
+  if (!all(names(sample_annotation) %in% names(df_long))){
     sample_annotation = sample_annotation %>% arrange_(order_column)
     #remove annotation columns, except the sample_id_column
     common_cols = intersect(names(sample_annotation), names(plot_df))
-    cols_to_remove = setdiff(common_cols, sample_id_column)
+    cols_to_remove = setdiff(common_cols, sample_id_col)
     plot_df = plot_df %>%
       select(-one_of(cols_to_remove))
     plot_df = plot_df %>%
-      merge(sample_annotation, by = sample_id_column)
+      merge(sample_annotation, by = sample_id_col)
   }
 
 
@@ -60,7 +76,7 @@ plot_peptide_level <- function(pep_name, data_df_long, sample_annotation,
 
 
   gg = ggplot(plot_df,
-              aes_string(x = order_column, y = measurement.col))
+              aes_string(x = order_column, y = measure_column))
   if (identical(geom, 'line')){
     gg = gg + geom_line(color = 'darkgrey', size = .3)
   }
@@ -96,10 +112,10 @@ plot_peptide_level <- function(pep_name, data_df_long, sample_annotation,
     if (is.data.frame(requant)){
       data_requant = requant
     } else {
-      if('requant' %in% names(data_df_long)){
+      if('requant' %in% names(df_long)){
         data_requant = plot_df %>% filter(requant)
       } else {
-        if('m_score' %in% names(data_df_long)){
+        if('m_score' %in% names(df_long)){
           data_requant = plot_df %>% filter(m_score == 2)
         } else{
           stop('requant cannot be plotted without requant table or m_scores!')
@@ -109,7 +125,7 @@ plot_peptide_level <- function(pep_name, data_df_long, sample_annotation,
     data_requant = data_requant %>%
       filter(rlang::UQ(as.name(feature_id_column)) %in% pep_name)
     gg = gg + geom_point(data = data_requant,
-                         aes_string(x = order_column, y = measurement.col),
+                         aes_string(x = order_column, y = measure_column),
                          color = 'red', size = .3, shape = 8)
   }
   if (theme == 'classic'){
@@ -118,123 +134,125 @@ plot_peptide_level <- function(pep_name, data_df_long, sample_annotation,
   return(gg)
 }
 
-#' Plot iRT peptides
-#'
-#' @param data_df_long - "openSWATH" format data frame
-#' @param sample_annotation
-#' @param batch_column
-#' @param feature_id_column
-#' @param measurement.col
-#' @param order_column
-#' @param sample_id_column
+#' @name feature_level_diagnostics
+#' @param spike_ins substring used to identify spike-in proteins in the column 'ProteinName'
 #' @param ... additional arguments to plot_peptide_level function
 #'
-#' @return
-#' @export
-#' @importFrom magrittr %>%
-#' @import dplyr
-#'
-#' @examples
-plot_iRTs <- function(data_df_long, sample_annotation,
-                      batch_column = 'MS_batch.final',
-                      sample_id_column = 'FullRunName',
-                      feature_id_column = 'peptide_group_label',
-                      measurement.col = 'Intensity',
-                      order_column = 'order',  ...){
-  iRT_peptides = data_df_long %>%
-    filter(grepl('iRT', ProteinName)) %>%
-    pull(feature_id_column)  %>% unique()
-  gg = plot_peptide_level(iRT_peptides, data_df_long, sample_annotation,
-                          batch_column = batch_column,
-                          feature_id_column = feature_id_column,
-                          measurement.col = measurement.col,
-                          sample_id_column = sample_id_column,
-                          order_column = order_column,
-                          title = 'iRT_peptides', ...)
-  return(gg)
-}
-
-#' Plot Spike-in peptides/proteins
-#'
-#' @param data_df_long
-#' @param sample_annotation
-#' @param spike_ins
-#' @param order_column
-#' @param measurement.col
-#' @param batch_column
-#' @param sample_id_column
-#' @param feature_id_column
-#' @param ... additional arguments to plot_peptide_level function
-#'
-#' @return
 #' @export
 #' @import dplyr
 #' @import ggplot2
 #' @importFrom magrittr %>%
 #'
 #' @examples
-plot_spike_ins <- function(data_df_long, sample_annotation,
-                           spike_ins = 'BOVIN',
+plot_spike_ins <- function(df_long, sample_annotation,
                            order_column = 'order',
-                           measurement.col = 'Intensity',
-                           batch_column = 'MS_batch',
+                           spike_ins = 'BOVIN',
                            sample_id_column = 'FullRunName',
-                           feature_id_column = 'peptide_group_label', ...){
-  spike_in_peptides = data_df_long %>%
+                           batch_column = 'MS_batch',
+                           measure_column = 'Intensity',
+                           feature_id_column = 'peptide_group_label',
+                           title = 'Spike-in BOVINE protein peptides', ...){
+  spike_in_peptides = df_long %>%
     filter(grepl(spike_ins, ProteinName)) %>%
     pull(feature_id_column) %>% unique()
-  gg = plot_peptide_level(spike_in_peptides, data_df_long, sample_annotation,
-                          batch_column,feature_id_column, measurement.col, order_column,
-                          title = 'Spike-in BOVINE protein peptides', ...)
+  gg = plot_peptide_level(spike_in_peptides, df_long = df_long,
+                          sample_annotation = sample_annotation,
+                          order_column = order_column,
+                          sample_id_col = sample_id_column,
+                          batch_column = batch_column, measure_column = measure_column,
+                          feature_id_column = feature_id_column,
+                          title = title, ...)
   return(gg)
 }
 
+#' @name feature_level_diagnostics
+#'
+#' @export
+#' @importFrom magrittr %>%
+#' @import dplyr
+#'
+#' @examples
+plot_iRTs <- function(df_long, sample_annotation,
+                      order_column = NULL,
+                      batch_column = 'MS_batch.final',
+                      sample_id_col = 'FullRunName',
+                      feature_id_column = 'peptide_group_label',
+                      measure_column = 'Intensity',
+                      title = 'iRT peptide profile', ...){
+  iRT_peptides = df_long %>%
+    filter(grepl('iRT', ProteinName)) %>%
+    pull(feature_id_column)  %>% unique()
+  gg = plot_peptide_level(iRT_peptides, df_long, sample_annotation,
+                          order_column = order_column,
+                          sample_id_col = sample_id_col,
+                          batch_column = batch_column,
+                          feature_id_column = feature_id_column,
+                          measure_column = measure_column,
+                          title = 'iRT_peptides', ...)
+  return(gg)
+}
+
+
+
 #' Plot Intensity for a few representative peptides for each step of the analysis including the fitting curve
 #'
-#' @param pep_name
-#' @param data_df_all_steps
+#' @param pep_name name of the peptide for diagnostic profiling
+#' @param data_df_all_steps data frame, similar to `df_long`,  where each row is a single feature in a single sample,
+#' at a certain step of the analysis (minimally raw and after linear normalization)
+#' thus it has minimally, `sample_id_col`, `feature_id_column`, `measure_column`  and `fit_step`,
+#' but usually also `m_score` (in OpenSWATH output result file)
 #' @param fit_df
-#' @param sample_annotation
 #' @param fit_value_var
-#' @param batch_column
-#' @param feature_id_column
-#' @param measurement.col
-#' @param sample_id_column
-#' @param order_column
-#' @param geom
+#' @param sample_annotation data matrix with 1) `sample_id_col` (this can be repeated as row names)
+#' 2) biological and 3) technical covariates (batches etc)
+#' @param order_column sample injection order
+#' @param sample_id_col name of the column in sample_annotation file,
+#' where the filenames (colnames of the data matrix are found)
+#' @param batch_column column in `sample_annotation` that should be used for batch comparison
+#' @param measure_column the column with expression/abundance/intensity.
+#' @param feature_id_column name of the column with feature/gene/peptide/protein ID
+#' used with long format matrix (`df_long`).
+#' Identical to wide format (`data_matrix`) to row name
+#' @param geom for the intensity (`measure_col`) profile:
 #' @param color_by_batch
 #' @param facet_by_batch
 #' @param title
 #' @param requant
 #' @param theme
-#' @param color_var
 #'
-#' @return
+#' @return `ggplot`-class plot with minimally two facets (before and after non-linear fit)
+#' with `measure_column` (Intensity) vs `order_column` (injection order)
+#' for selected peptides (specified in `pep_name`)
 #' @export
 #' @import ggplot2
 #'
 #' @examples
-plot_with_fitting_curve <- function(pep_name, data_df_all_steps, fit_df, sample_annotation,
+plot_with_fitting_curve <- function(pep_name, data_df_all_steps,
+                                    sample_annotation,
+                                    fit_df,
                                     fit_value_var = 'fit', fit_step = '3_loess_fit',
+                                    order_column = NULL,
+                                    sample_id_col = 'FullRunName',
                                     batch_column = 'MS_batch',
+                                    measure_column = 'Intensity',
                                     feature_id_column = 'peptide_group_label',
-                                    measurement.col = 'Intensity',
-                                    sample_id_column = 'FullRunName',
-                                    order_column = NULL, geom = c('point', 'line'),
+                                    geom = c('point', 'line'),
                                     color_by_batch = F, facet_by_batch = F,
-                                    title = NULL, requant = NULL, theme = 'classic',
-                                    color_var = 'fit'){
+                                    title = NULL, requant = NULL,
+                                    theme = 'classic'){
+  #TODO: in non-linear fit, change "Intensity_normalized" to "Intensity", but rename "Intensity" as "Intensity_before_fit"
   if(length(pep_name) > 10){
     warning("Visualisation of individual features can be suboptimal,
             consider exploring no more than 5 features at a time")
   }
-  gg = plot_peptide_level(pep_name, data_df_long = data_df_all_steps,
+  gg = plot_peptide_level(pep_name, df_long = data_df_all_steps,
                           sample_annotation = sample_annotation,
-                          sample_id_column = sample_id_column,
+                          order_column = order_column,
+                          sample_id_col = sample_id_col,
                           batch_column = batch_column,
+                          measure_column = measure_column,
                           feature_id_column = feature_id_column,
-                          measurement.col = measurement.col,
-                          order_column = order_column, title = title,
+                          title = title,
                           facet_by_batch = facet_by_batch)
   if(!("Step" %in% names(fit_df))){
     fit_df$Step = fit_step
@@ -244,7 +262,7 @@ plot_with_fitting_curve <- function(pep_name, data_df_all_steps, fit_df, sample_
   }
   fit_df = fit_df %>%
     filter(rlang::UQ(as.name(feature_id_column)) %in% pep_name) %>%
-    merge(sample_annotation, by = c(sample_id_column, batch_column))
+    merge(sample_annotation, by = c(sample_id_col, batch_column))
   if(identical(color_by_batch, FALSE)){
     gg = gg + geom_line(data = fit_df,
                         aes_string(y = fit_value_var, x = order_column, group = batch_column),
