3978c91d8fa7ffff0dd241788c04deb3d9ce7893
Jelena Chuklina <chuklina.jelena@gmail.com>
2018-05-06 19:41:15 +0200

    added PCA and PVCA plot
    Former-commit-id: c607eb1adb05be7d5570312b18e8d27a9888d6d3 [formerly 7dcae3b3864c87256b5f910d1977a2eb4289f3ee]
    Former-commit-id: 5cfe4e94580c9bc04b0f03b0ff6539a8039f65d3 [formerly de99c97c2d4c6bd622513972b085264f528cd77b] [formerly 1944fd6c74f6d0af5c1bea1c57211c325c992030]
    Former-commit-id: d00dc154843ef95d9699b7cf9d16a8e4551a5750
    Former-commit-id: ca9dd1984b28493a1efe01bdda6cc81c22eee640

--- R/genome_wide_diagnostics.R
+++ R/genome_wide_diagnostics.R
@@ -1,5 +1,18 @@
+#' @param sample_annotation data matrix with 1) `sample_id_col` (this can be repeated as row names) 2) biological and 3) technical covariates (batches etc)
+#' @param sample_id_col name of the column in sample_annotation file,
+#' where the filenames (colnames of the data matrix are found)
+#' @param batch_column column in `sample_annotation` that should be used for batch comparison
+#' @param measure_column if `df_long` is among the parameters, it is the column with expression/abundance/intensity,
+#' otherwise, it is used internally for consistency
+#' @param df_long data frame where each row is a single feature in a single sample,
+#' thus it has minimally, `sample_id_col`, `feature_id_column` and `measure_column`, but usually also `m_score` (in OpenSWATH output result file)
+#' @param feature_id_column name of the column with feature/gene/peptide/protein ID used with long format matrix (`df_long`). In wide format (`data_matrix`) this would be the row name
+#' @name genome_wide_diagnostics
+
+
 #' Plot the sample average
 #'
+#' @name genome_wide_diagnostics
 #' @param data_matrix
 #' @param sample_annotation
 #' @param sample_id_col
@@ -144,4 +157,153 @@ cluster_samples <- function(data_matrix, color_df, plot_title, ...){
   plotDendroAndColors(hierarchical_clust, color_df, rowTextAlignment = 'left',
                       main = plot_title,
                       hang = -0.1, addGuide = T, ...)
+}
+
+#' Quantify variance distribution by variable
+#'
+#' @param data_matrix
+#' @param sample_annotation
+#' @param technical_covariates
+#' @param biological_covariates
+#' @param plot_title
+#' @param colors_for_bars
+#' @param threshold_pca
+#' @param threshold_var
+#'
+#' @return
+#' @export
+#' @import ggplot2
+#' @importFrom pvca pvcaBatchAssess
+#'
+#' @examples
+plot_pvca <- function(data_matrix, sample_annotation, sample_id_column = 'FullRunName',
+                 technical_covariates = NULL, biological_covariates = NULL,
+                 plot_title = NULL, colors_for_bars = NULL, threshold_pca = .6,
+                 threshold_var = .01, theme = 'classic'){
+  factors_for_PVCA = c(technical_covariates, biological_covariates)
+  if (!is.null(sample_id_column)){
+    if(sample_id_column %in% names(sample_annotation)){
+      rownames(sample_annotation) = sample_annotation[[sample_id_column]]
+    }
+  }
+
+  tech_interactions = expand.grid(technical_covariates, technical_covariates) %>%
+    mutate(tech_interaction = paste(Var1, Var2, sep = ':')) %>%
+    pull(tech_interaction)
+  biol_interactions = expand.grid(biological_covariates, biological_covariates) %>%
+    mutate(biol_interactions = paste(Var1, Var2, sep = ':')) %>%
+    pull(biol_interactions)
+
+  if(!(all(rownames(sample_annotation) %in% colnames(data_matrix)))){
+    stop('check data matrix column names or these in sample annotation')
+  }
+  covrts.annodf = Biobase::AnnotatedDataFrame(data=sample_annotation)
+  expr_set = Biobase::ExpressionSet(data_matrix[,rownames(sample_annotation)], covrts.annodf)
+  pvcaAssess <- pvcaBatchAssess (expr_set, factors_for_PVCA, threshold = threshold_pca)
+  pvcaAssess_df = data.frame(weights = as.vector(pvcaAssess$dat),
+                             label = pvcaAssess$label,
+                             stringsAsFactors = F)
+
+  label_of_small = sprintf('Below %1.0f%%', 100*threshold_var)
+  if (sum(pvcaAssess_df$weights < threshold_var) > 1){
+    pvca_res_small = sum(pvcaAssess_df$weights[pvcaAssess_df$weights < threshold_var])
+    pvca_res = pvcaAssess_df[pvcaAssess_df$weights >= threshold_var, ]
+    pvca_res_add = data.frame(weights = pvca_res_small, label = label_of_small)
+    pvca_res = rbind(pvca_res, pvca_res_add)
+  }
+  else {pvca_res = pvcaAssess_df}
+
+  technical_covariates = c(technical_covariates, tech_interactions)
+  biological_covariates = c(biological_covariates, biol_interactions)
+  pvca_res = pvca_res %>% mutate(category = ifelse(label %in% technical_covariates, 'technical',
+                                                   ifelse(label %in% biological_covariates, 'biological',
+                                                          ifelse(label %in% c(label_of_small, 'resid'), 'residual', 'biol:techn'))))
+
+  pvca_res = pvca_res %>%
+    arrange(desc(weights)) %>%
+    arrange(label == label_of_small) %>%
+    arrange(label == 'resid')
+
+  pvca_res$label = factor(pvca_res$label, levels = pvca_res$label)
+
+  y_title = 'Weighted average proportion variance'
+  gg  = ggplot(pvca_res, aes(x = label, y = weights, fill = category))+
+    geom_bar(stat = 'identity', color = 'black', size = 1.5)+
+    ylab(y_title)
+  if(theme == 'classic'){
+    gg = gg +theme_classic()
+  }
+  if(is.null(colors_for_bars)){
+    colors_for_bars = c('grey', wesanderson::wes_palettes$Rushmore[3:5])
+    names(colors_for_bars) = c('residual', 'biological', 'biol:techn', 'technical')
+    gg = gg + scale_fill_manual(values = colors_for_bars)
+  }
+  gg = gg +
+    theme(axis.title.x = NULL, axis.text.x = element_text(angle = 90, hjust = 1, vjust = .5))+
+    xlab(NULL)+
+    theme(text = element_text(size=15))+
+    guides(fill=guide_legend(override.aes=list(color=NA), title=NULL))
+  return(list(plot =gg, df = pvca_res))
+}
+
+#' plot PCA plot
+#'
+#' @param data_matrix
+#' @param sample_annotation
+#' @param factor_to_color
+#' @param PC_to_plot
+#' @param theme
+#'
+#' @return
+#' @export
+#' @import ggplot2
+#' @import ggfortify
+#'
+#' @examples
+plot_pca <- function(data_matrix, sample_annotation, factor_to_color,
+                     PC_to_plot = c(1,2), colors_for_factor = NULL, theme = 'classic'){
+  gg = autoplot(prcomp(t(data_matrix)), data = sample_annotation, colour = factor_to_color,
+           x = PC_to_plot[1], y = PC_to_plot[2])
+  if (theme == 'classic'){
+    gg = gg + theme_classic()
+  }
+  if(!is.null(colors_for_factor)){
+    gg = gg + scale_color_manual(values = colors_for_factor)
+  }
+  return(gg)
+}
+
+#' Plot the heatmap of samples
+#'
+#' @param data_matrix
+#' @param sample_annotation
+#' @param fill_the_missing
+#' @param cluser_rows
+#' @param cluster_cols
+#' @param annotation_color_list
+#' @param filename
+#' @param ...
+#'
+#' @return
+#' @export
+#' @import pheatmap
+#'
+#' @examples
+plot_heatmap <- function(data_matrix, sample_annotation, fill_the_missing = T,
+                         cluser_rows = F, cluster_cols = F,
+                         annotation_color_list = NA,
+                         filename = NA,
+                         ...){
+  if(fill_the_missing) {
+    data_matrix[is.na(data_matrix)] = 0
+    heatmap_color = c('black', colorRampPalette(rev(RColorBrewer::brewer.pal(n = 7, name = "RdYlBu")))(100))
+  }
+  else {
+    heatmap_color = colorRampPalette(rev(RColorBrewer::brewer.pal(n = 7, name = "RdYlBu")))(100)
+  }
+  p <- pheatmap(data_matrix, cluster_rows = cluser_rows, cluster_cols = cluster_cols,
+           color = heatmap_color,
+           annotation_col = sample_annotation, annotation_colors = color_list,
+           filename = filename, ...)
+  return(p)
 }
