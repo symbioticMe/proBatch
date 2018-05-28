d9e8e0671783ca9cf892fb1ec178ba27ec70e2c0
Jelena Chuklina <chuklina.jelena@gmail.com>
2018-05-28 16:54:30 +0200

    created documentation for datasets + stubs for functions that created this data
    Former-commit-id: 6bde00016dd9677645fa452944a3207595dadd03 [formerly 0fae46e99abd376c31ba26c78d70082674d4ae91] [formerly c689928558a0f69ce84ac1e01620640fd484cfda]
    Former-commit-id: 6d6ccc10f45dac5bfd238f06a9fb7b39aca8b8de
    Former-commit-id: 1046675d0cf2dc70247ea342c6a5747ef7a8167c

---
+++ R/data.R
@@ -0,0 +1,55 @@
+#' Sample annotation data version 1
+#'
+#' This is data from Evan's aging study with mock instruments to show how
+#' instrument-specific functionality works
+#'
+#' @format A data frame with 375 rows and 18 variables:
+#' \describe{
+#'   \item{FullRunName}{name of the file, in most functions used for `sample_id_col`}
+#'   \item{MS_batch.final}{mass-spectrometry batch: 7-level factor of manually annotated batches}
+#'   \item{EarTag}{mouse ID, i.e. ID of the biological object}
+#'   \item{Strain}{mouse strain ID - biological covariate #1}
+#'   \item{Diet}{diet - either `HFD` = `High Fat Diet` or `CD` = `Chow Diet`.
+#'   `Mix` stands for mixture of several samples}
+#'   \item{Sex}{mice sex - 3-level biological covariate. Possible values - ''}
+#'   \item{Age_Days}{mice age at sampling - numeric biological covariate}
+#'   \item{RunDate}{mass-spectrometry running date. In combination with `RunTime` used for running order determination}
+#'   \item{RunTime}{mass-spectrometry running time. In combination with `RunDate` used for running order determination}
+#'   \item{SacrificeDate}{date of mouse sacrifice - technical covariate}
+#'   \item{ProteinPrepDate}{date of protein preparation - technical covariate}
+#'   ...
+#' }
+"example_sample_annotation1"
+
+#' Example protein data
+#'
+#' This is data from Evan's aging study with all iRT, spike-in peptides,
+#' few random peptides and QTL proteins for biological signal improvement demonstration
+#'
+#' @format A data frame with 200625 rows and 8 variables:
+#' \describe{
+#'   \item{peptide_group_label}{peptide ID, which is regular feature level. This column is mostly used as `feature_id_col`}
+#'   \item{RT}{retention time. Relevant to identify retention time related bias}
+#'   \item{Intensity}{peptide group intensity in given sample. Used in function as `measure_col`}
+#'   \item{ProteinName}{Protein group ID, specified as N/UniProtID1|UniProtID2|...,
+#'   where N is number of protein peptide group maps to. If 1/UniProtID, then this is proteotypic peptide}
+#'   \item{assay_rt}{retention time as in DIA library}
+#'   \item{m_score}{peptide group identification FDR as determined by pyProphet}
+#'   \item{FullRunName}{name of the file, in most functions used for `sample_id_col`}
+#'   #'   ...
+#' }
+#' @source PRIDE ID will be added in future
+"example_proteome"
+
+#' Peptide annotation data
+#'
+#' This is data from Evan's aging study annotated with gene names
+#'
+#' @format A data frame with 200625 rows and 8 variables:
+#' \describe{
+#'   \item{Peptide}{peptide group label ID, identical to `peptide_group_label` in `example_proteome`}
+#'   \item{Gene}{HUGO gene ID}
+#'   \item{ProteinName}{protein group name as specified in `example_proteome`}
+#'   ... other parameters determined by 'summarize_peptides' function
+#'   }
+"example_peptide_annotation"
