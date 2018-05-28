6bde00016dd9677645fa452944a3207595dadd03
Jelena Chuklina <chuklina.jelena@gmail.com>
2018-05-28 16:54:30 +0200

    created documentation for datasets + stubs for functions that created this data
    Former-commit-id: c689928558a0f69ce84ac1e01620640fd484cfda
    Former-commit-id: 0fae46e99abd376c31ba26c78d70082674d4ae91

---
+++ R/create_peptide_annotation.R
@@ -0,0 +1,8 @@
+library(tidyverse)
+library(proBatch)
+
+#load the peptide annotation from Evan
+
+#load proteome, summarize on peptide level, remove non-proteotypic peptides
+
+#merge two dataframes
