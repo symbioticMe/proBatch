b615a07e838780dab505f2e3b53cc620218d98c3
Jelena Chuklina <chuklina.jelena@gmail.com>
2018-05-28 16:54:30 +0200

    created documentation for datasets + stubs for functions that created this data
    Former-commit-id: 0aa0df1a428dd2fa11429a3e03b4c54add857f62 [formerly 6d2ab71ff09f6df718df55f29ba94e82ec588fe4] [formerly e831271df80e5fb47f3eed40960831ba4541f7af]
    Former-commit-id: d9a7a8cb576d8da8f277b7bcfb4296c6efbb116b
    Former-commit-id: fe140c0431a37ebb738385a851efbe93d8727def

---
+++ data-raw/create_example_proteome.R
@@ -0,0 +1,14 @@
+library(tidyverse)
+library(proBatch)
+
+#load the full proteome
+
+#extract the iRTs
+
+#extract the Bovine sample
+
+#extract the QTL proteins
+
+#extract random peptides
+
+devtools::use_data(example_proteome)
