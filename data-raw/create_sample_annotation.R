19b1c0630a00b2bd38e7871485345c90183cd270
Jelena Chuklina <chuklina.jelena@gmail.com>
2018-05-28 16:54:30 +0200

    created documentation for datasets + stubs for functions that created this data
    Former-commit-id: d2a6921c30e3fde08bd693319cd51a578198b51f [formerly 8aaab6e119e3d781beec55eccd1014c249fb52fb]
    Former-commit-id: cac57c6eeab8ae0693d89e58faa0d257dfd837c9 [formerly 986edcf3539fadabd567d635a2023cab0242ac46] [formerly 08f9419927dbdc123bcaee20f74446c36c3e0beb]
    Former-commit-id: 6eeb565ab2e1f09673ffe82ab8f99bb512eb79fe
    Former-commit-id: 513e287025a8b6beef7959a196efebd862810766

---
+++ data-raw/create_sample_annotation.R
@@ -0,0 +1,21 @@
+library(proBatch)
+library(tidyverse)
+library(reshape2)
+
+data("example_proteome", package = 'proBatch')
+data("example_sample_annotation", package = 'proBatch')
+sample_annotation <- read.csv("~/Dropbox/batch_effects/data/sample_annotation.csv")
+
+example_sample_annotation1 = sample_annotation %>%
+  select(FullRunName, MS_batch.final) %>% merge(example_sample_annotation)
+
+#add instruments
+example_sample_annotation1 = example_sample_annotation1 %>%
+  mutate(instrument = ifelse(MS_batch.final %in% c('Batch_1','Batch_2'), 'Instr_A',
+                             ifelse(MS_batch.final %in% c('Batch_3', 'Batch_4', 'Batch_5'),
+                                    'Instr_B', 'Instr_C')))
+
+set.seed(1)
+example_sample_annotation1$instrument =
+  sample(example_sample_annotation1$imaginary_instrument)
+
