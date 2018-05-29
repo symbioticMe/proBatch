64a618c0c8061e0d4f0ea43fca898ca17376e71f
Jelena Chuklina <chuklina.jelena@gmail.com>
2018-05-29 15:31:15 +0200

    (untested) stub for creation of example proteome
    Former-commit-id: bb486220654e4746e65e59843c9e016dd20f525c [formerly a333b7513fbf42289ac88d7ec7929e9c317be2e6] [formerly f5e5928aa3230d4d8ce150b628a1b4069361e793]
    Former-commit-id: 6dafcda51724d4e3d9d980a2bde595d9fd452904
    Former-commit-id: 8938eabcce94f914cea78c85d9a723feab0ae699

--- data-raw/create_example_proteome.R
+++ data-raw/create_example_proteome.R
@@ -1,14 +1,37 @@
 library(tidyverse)
+library(readr)
 library(proBatch)
 
+peptide_annotation = read_csv('data/peptide_annotation.csv')
+
 #load the full proteome
-
-#extract the iRTs
-
-#extract the Bovine sample
+proteome <- read_csv("data/OpenSWATH_proteome.csv")
 
 #extract the QTL proteins
+QTLproteins = c("Cat", "Glo1", "Cox7a2l", "Lrpprc", "Pm20d1" , "Dhtkd1" , "Haao" ,
+                "Tpmt", "Hdhd3" , "D2hgdh" )
 
-#extract random peptides
+#extract a hanful of proteins with overfitting problem
+overfitting_prots = c('Mtch1','Cyfip1', 'Nnt', 'Gclm')
+
+#unite proteins for subsampling
+proteins_to_subsample = union(QTLproteins, overfitting_prots)
+
+#subsample the selected proteins and iRTs + Bovine
+#extract the spike-ins: iRTs and Bovine:
+peptides_sub = peptide_annotation %>%
+  filter((Gene %in% proteins_to_subsample) |
+           grepl('irt', ProteinName, ignore.case = T) |
+           grepl('bovine', Gene, ignore.case = T)) %>%
+  pull(Peptide)
+
+#sample random peptides
+random_pep = sample_random_peptides(proteome)
+
+#unite peptides for subsampling
+peptides_sub = union(peptides_sub, random_pep$peptides)
+
+#filter for peptides selected for subsampling
+example_proteome = proteome %>% filter(peptide_group_label %in% peptides_sub)
 
 devtools::use_data(example_proteome)
