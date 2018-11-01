1179f15d743f042bb1087539582a388729e0e771
Chloe H. Lee <hjleeab@gmail.com>
2018-11-01 10:41:19 +0100

    update SWATH normalization with fread loading, log_transform()
    Former-commit-id: da425cb15bc722e64298fe10e69ff493f32f209e [formerly db234fe5284afb5d85e820901f5f3f7425dce482]
    Former-commit-id: ea9dc3d725bdf21c1e6563b72c3c29b047c0ffe1 [formerly 4754f00f8b83fd3222391101c175e3903c00d1d4] [formerly ee5a77b86acefd0c6280bf64d5c7416fb0e2b027]
    Former-commit-id: ffa9ee7df1b75353fec3f31fdfcab25f7d450c20
    Former-commit-id: d83eef01976c7e94739dc530cf6ff25da95fde53

--- Workflow_openSWATH data v3_final.R
+++ Workflow_openSWATH data v3_final.R
@@ -1,9 +1,19 @@
 # FullDataSet Finalized normalization workflow for Evan's analysis #
 
-library(readr) #this is full Evan's dataset
-Fullruns_dataset = read_delim("S:/html/E1801171630_feature_alignment_requant.tsv", "\t", escape_double = FALSE)
-Fullruns_dataset = Fullruns_dataset %>% mutate(FullRunName = gsub('/scratch/55808263.tmpdir/wevan_(.+)\\.mzXML\\.gz', '\\1', filename))
-  # consider using fread if this changes dataset in any way 
+# Install dependencies if necessary 
+bioc_deps <- c("GO.db", "preprocessCore", "impute", "sva", "pvca")
+cran_deps <- c("roxygen2","lubridate","tidyverse","reshape2","lazyeval","readr","WGCNA", "rlang", 
+               "corrplot","ggfortify","pheatmap","dplyr","data.table","wesanderson")
+source("http://bioconductor.org/biocLite.R") 
+biocLite(bioc_deps); 
+install.packages(cran_deps); 
+lapply(bioc_deps, require, character.only = TRUE)
+lapply(cran_deps, require, character.only = TRUE)
+
+# Loading data
+Fullruns_dataset = as.data.frame(fread(file = 'S:/E1801171630_feature_alignment_requant.tsv', sep = '\t', header = TRUE))
+Fullruns_dataset = Fullruns_dataset %>% 
+  mutate(FullRunName = gsub('/scratch/55808263.tmpdir/wevan_(.+)\\.mzXML\\.gz', '\\1', filename))
 
 # Peptide annotation  
 peptide_annotation = read.csv("S:/html/peptide_annotations_6600evosep.csv")
@@ -17,16 +27,18 @@ sample_annotation = date_to_sample_order (sample_annotation,
                                           order_col = 'order',
                                           instrument_col = NULL)
 
-  # check if any pre-processing step is required for Evan's dataset e.g. requants/ peptide of missing batch etc 
-
 # Generate data_matrix from SWATH_long
-SWATH_matrix = convert_to_matrix(Allruns_requant, feature_id_col = 'peptide_group_label',
+SWATH_matrix = convert_to_matrix(Fullruns_dataset, feature_id_col = 'peptide_group_label',
                                  measure_col = 'Intensity',
                                  sample_id_col = 'FullRunName')
 
+# For Evan's data, no requant nor peptide filtering are executed. 
+# All normalization and batch correction have their intrinsic filtering system for those peptides 
+# not suitable for fitting/ normalization/ batch correction. Full dataset 
+
 ############################### Normalization & Batch correction ####################################################
 # log2 transformation of data_matrix 
-SWATH_matrix_log2 = log2(SWATH_matrix + 1) 
+SWATH_matrix_log2 = log_transform(SWATH_matrix)
 
 # Quantile normalization of the log2 transformed data 
 SWATH_matrix_qnorm = quantile_normalize(SWATH_matrix_log2)
