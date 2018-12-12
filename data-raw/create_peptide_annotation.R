d2ff035fffa66a4457ede6640110735d34a816d7
Jelena Chuklina <chuklina.jelena@gmail.com>
2018-12-12 18:55:53 +0100

    deleted the scripts specifying, how the example data file has been created
    Former-commit-id: d4880de77915ab8e30c3897788ff81b3de154d38

--- data-raw/create_peptide_annotation.R
+++
@@ -1,8 +0,0 @@
-library(tidyverse)
-#library(proBatch)
-
-#load the peptide annotation from Evan
-
-#load proteome, summarize on peptide level, remove non-proteotypic peptides
-
-#merge two dataframes
