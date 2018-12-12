b14867d19272e3f98b491a3159dc034f61708991
Jelena Chuklina <chuklina.jelena@gmail.com>
2018-12-12 18:56:21 +0100

    updated vignette docs
    Former-commit-id: 88bf8b1e336f66838ecb1f79ad4b3bfaad75057c

--- vignettes/proBatch_vignette.R
+++ vignettes/proBatch_vignette.R
@@ -8,10 +8,10 @@ knitr::opts_chunk$set(
   comment = "#>"
 )
 
-## ---- include = TRUE, fig.align = "center", echo=FALSE, fig.cap="proBatch in batch correction workflow", out.width = '50%'----
+## ----batch_workflow, include = TRUE, fig.align = "center", echo=FALSE, fig.cap="proBatch in batch correction workflow", out.width = '50%'----
 knitr::include_graphics("Batch_effects_workflow_staircase.png")
 
-## ---- eval = FALSE-------------------------------------------------------
+## ----dependencies, eval = FALSE------------------------------------------
 #  bioc_deps <- c("GO.db", "impute", "preprocessCore", "pvca","sva" )
 #  cran_deps <- c("corrplot", "data.table", "ggfortify","lazyeval", "pheatmap", "reshape2",
 #                 "rlang", "tidyverse","wesanderson","WGCNA")
@@ -19,34 +19,34 @@ knitr::include_graphics("Batch_effects_workflow_staircase.png")
 #  biocLite(bioc_deps)
 #  install.packages(cran_deps)
 
-## ---- fig.show='hold', eval = FALSE--------------------------------------
+## ----install_proBatch, fig.show='hold', eval = FALSE---------------------
 #  # Once the proBatch package is in Bioconductor, can easily install by:
 #  install.packages("proBatch")
 #  
 #  # Alternatively, install the development version from GitHub:
 #  install.packages("devtools")
-#  devtools::install_github("symbioticMe/proBatch")
+#  devtools::install_github("symbioticMe/proBatch", build_vignettes = TRUE)
 
-## ------------------------------------------------------------------------
+## ----load_tidyverse------------------------------------------------------
 require(tidyverse)
 
-## ------------------------------------------------------------------------
+## ----col_names-----------------------------------------------------------
 feature_id_col = 'peptide_group_label'
 measure_col = 'Intensity'
 sample_id_col = 'FullRunName'
 essential_columns = c(feature_id_col, measure_col, sample_id_col)
 
-## ------------------------------------------------------------------------
+## ----tech_bio_cols-------------------------------------------------------
 technical_covariates = c('MS_batch', 'digestion_batch', 'RunDate', 'RunTime')
 biological_covariates = c('Strain', 'Diet', 'Sex', 'Age_Days')
 biospecimen_id_col = "EarTag"
 
-## ---- fig.show='hold'----------------------------------------------------
+## ----load_data, fig.show='hold'------------------------------------------
 library(proBatch)
 data("example_proteome", "example_sample_annotation", "example_peptide_annotation", 
      package = "proBatch")
 
-## ---- fig.show='hold'----------------------------------------------------
+## ----date_to_order, fig.show='hold'--------------------------------------
 generated_sample_annotation <- date_to_sample_order(example_sample_annotation,
                                           time_column = c('RunDate','RunTime'),
                                           new_time_column = 'generated_DateTime',
@@ -57,7 +57,7 @@ library(knitr)
 kable(generated_sample_annotation[1:5,] %>%
   select(c("RunDate", "RunTime", "order", "generated_DateTime", "generated_order")))
 
-## ---- fig.show='hold'----------------------------------------------------
+## ----pep_annotation, fig.show='hold'-------------------------------------
 generated_peptide_annotation <- create_peptide_annotation(example_proteome, 
                                         feature_id_col = 'peptide_group_label',
                                         annotation_col = c('ProteinName', 'Gene'))
@@ -66,7 +66,7 @@ generated_peptide_annotation <- create_peptide_annotation(example_proteome,
 example_proteome = example_proteome %>% select(one_of(essential_columns))
 gc()
 
-## ------------------------------------------------------------------------
+## ----long_to_matrix------------------------------------------------------
 example_matrix <- long_to_matrix(example_proteome)
 
 ## ------------------------------------------------------------------------
@@ -108,7 +108,7 @@ plot_sample_mean(quantile_normalized_matrix, example_sample_annotation,
                  color_by_batch = T, ylimits = c(12, 16), 
                  color_scheme = color_list[[batch_col]])
 
-## ---- fig.show='hold', fig.width=10, fig.height=5------------------------
+## ----plot_hierarchical, fig.show='hold', fig.width=10, fig.height=5------
 color_annotation <- color_scheme$color_df
 selected_annotations <- c("MS_batch",  "digestion_batch", "Diet")
 #select only a subset of samples for plotting
@@ -119,14 +119,14 @@ plot_hierarchical_clustering(quantile_normalized_matrix, color_annotation,
                              distance = "euclidean", agglomeration = 'complete',
                              label_samples = F)
 
-## ---- fig.show='hold', fig.width=10, fig.height=11-----------------------
+## ----plot_heatmap, fig.show='hold', fig.width=10, fig.height=11----------
 plot_heatmap(quantile_normalized_matrix, example_sample_annotation, 
              sample_annotation_col = selected_annotations, 
              cluster_cols = T, 
              annotation_color_list = color_scheme$list_of_colors,
              show_rownames = F, show_colnames = F)
 
-## ---- fig.show='hold',  fig.width=3.4, fig.height=2.3--------------------
+## ----plot_PCA, fig.show='hold',  fig.width=3.4, fig.height=2.3-----------
 plot_PCA(quantile_normalized_matrix, example_sample_annotation, color_by = 'MS_batch', 
               plot_title = "MS batch", colors_for_factor = color_list[[batch_col]])
 plot_PCA(quantile_normalized_matrix, example_sample_annotation, color_by = "digestion_batch", 
@@ -134,7 +134,7 @@ plot_PCA(quantile_normalized_matrix, example_sample_annotation, color_by = "dige
 plot_PCA(quantile_normalized_matrix, example_sample_annotation, color_by = "Diet",  
          plot_title = "Diet", colors_for_factor = color_list[["Diet"]])
 
-## ---- fig.show='hold', eval = FALSE--------------------------------------
+## ----plot_PVCA, fig.show='hold', eval = FALSE----------------------------
 #  pvca <- plot_PVCA(quantile_normalized_matrix, example_sample_annotation,
 #                    technical_covariates = c('MS_batch', 'digestion_batch'),
 #                    biological_covariates = c(biological_covariates, biospecimen_id_col))
@@ -142,7 +142,7 @@ plot_PCA(quantile_normalized_matrix, example_sample_annotation, color_by = "Diet
 ## ---- include = TRUE, fig.align = "center", echo=FALSE, out.width = '80%'----
 knitr::include_graphics("pvca_quantile_normalized.png")
 
-## ---- fig.show='hold'----------------------------------------------------
+## ----plot_spikeIn, fig.show='hold'---------------------------------------
 quantile_normalized_long <- matrix_to_long(quantile_normalized_matrix, example_sample_annotation)
 plot_spike_in(quantile_normalized_long, example_sample_annotation, 
               peptide_annotation = generated_peptide_annotation,
@@ -151,11 +151,11 @@ plot_spike_in(quantile_normalized_long, example_sample_annotation,
               color_by_batch = T, color_scheme = color_list[[batch_col]])
 
 
-## ---- fig.show='hold', fig.width=5, fig.height=2.4-----------------------
+## ----loess_fit, fig.show='hold', fig.width=5, fig.height=2.4-------------
 loess_fit <- adjust_batch_trend(quantile_normalized_matrix, example_sample_annotation)
 loess_fit_matrix <- loess_fit$data_matrix
 
-## ---- fig.show='hold', fig.width=5, fig.height=2.4-----------------------
+## ----loess_30, fig.show='hold', fig.width=5, fig.height=2.4--------------
 loess_fit_30 <- adjust_batch_trend(quantile_normalized_matrix, example_sample_annotation, span = 0.3)
 
 quantile_normalized_long <- matrix_to_long(quantile_normalized_matrix)
@@ -164,38 +164,38 @@ plot_with_fitting_curve(pep_name = "10231_QDVDVWLWQQEGSSK_2",
             color_by_batch = T, color_scheme = color_list[[batch_col]],
             fit_df = loess_fit_30$fit_df, plot_title = "Span = 30%")
 
-## ---- fig.show='hold', fig.width=5, fig.height=2.4-----------------------
+## ----loess_70, fig.show='hold', fig.width=5, fig.height=2.4--------------
 loess_fit_70 <- adjust_batch_trend(quantile_normalized_matrix, example_sample_annotation, span = 0.7)
 plot_with_fitting_curve(pep_name = "10231_QDVDVWLWQQEGSSK_2", 
             df_long = quantile_normalized_long, example_sample_annotation, 
             color_by_batch = T, color_scheme = color_list[[batch_col]],
             fit_df = loess_fit_70$fit_df, plot_title = "Span = 70%")
 
-## ------------------------------------------------------------------------
+## ----loess_to_long-------------------------------------------------------
 loess_fit_df <- matrix_to_long(loess_fit_matrix)
 
-## ---- fig.show='hold', fig.width=3, fig.height=2.4-----------------------
+## ----median_batch, fig.show='hold', fig.width=3, fig.height=2.4----------
 peptide_median_df <- center_peptide_batch_medians(loess_fit_df, example_sample_annotation)
 plot_single_feature(pep_name = "46213_NVGVSFYADKPEVTQEQK_2", df_long = peptide_median_df, 
             example_sample_annotation, color_by_col = NULL, measure_col = 'Intensity_normalized',
             plot_title = "Feature-level Median Centered")
 
-## ---- fig.show='hold'----------------------------------------------------
+## ----comBat, fig.show='hold'---------------------------------------------
 comBat_matrix <- correct_with_ComBat(loess_fit_matrix, example_sample_annotation)
 
-## ---- fig.show='hold',  fig.width=3, fig.height=2.4----------------------
+## ----combat_result, fig.show='hold',  fig.width=3, fig.height=2.4--------
 combat_df <- matrix_to_long(comBat_matrix)
 plot_single_feature (pep_name = "46213_NVGVSFYADKPEVTQEQK_2", loess_fit_df, 
           example_sample_annotation, plot_title = "Loess Fitted", color_by_col = NULL)
 plot_single_feature (pep_name = "46213_NVGVSFYADKPEVTQEQK_2", combat_df, 
           example_sample_annotation, plot_title = "ComBat corrected", color_by_col = NULL)
 
-## ---- fig.show='hold'----------------------------------------------------
+## ----batch_corr_general, fig.show='hold'---------------------------------
 batch_corrected_matrix <- correct_batch_effects(data_matrix = quantile_normalized_matrix, 
                                   example_sample_annotation, discreteFunc = 'ComBat',
                                   abs.threshold = 5, pct.threshold = 0.20)
 
-## ---- fig.show='hold', fig.height=5, fig.width=8-------------------------
+## ----setup_corr_heatmap, fig.show='hold', fig.height=5, fig.width=8------
 earTags <- c("ET1524", "ET2078", "ET1322", "ET1566", "ET1354", "ET1420", "ET2154",
              "ET1515", "ET1506", "ET2577", "ET1681", "ET1585", "ET1518", "ET1906")
 
@@ -216,14 +216,14 @@ breaksList <- seq(0.7, 1, by = 0.01) # color scale of pheatmap
 heatmap_colors = colorRampPalette(
   rev(RColorBrewer::brewer.pal(n = 7, name = "RdYlBu")))(length(breaksList))
 
-## ---- fig.show='hold', fig.height=4, fig.width=6-------------------------
+## ----corr_samples_heatmap, fig.show='hold', fig.height=2.12, fig.width=3.35----
 # Plot the heatmap 
 plot_sample_corr_heatmap(quantile_normalized_matrix, samples_to_plot = replicate_filenames, 
                          flavor = 'pheatmap', plot_title = 'Quantile Normalized', 
                          annotation_colors = color_list[factors_to_show], 
                          annotation_col = replicate_annotation,
                          color = heatmap_colors, breaks = breaksList, 
-                         cluster_rows=F, cluster_cols=F,fontsize = 7,
+                         cluster_rows=F, cluster_cols=F,fontsize = 4,
                          annotation_names_col = TRUE, annotation_legend = FALSE, 
                          show_colnames = F)
 
@@ -232,11 +232,11 @@ plot_sample_corr_heatmap(batch_corrected_matrix, samples_to_plot = replicate_fil
                          annotation_colors = color_list[factors_to_show], 
                          annotation_col = replicate_annotation,
                          color = heatmap_colors, breaks = breaksList, 
-                         cluster_rows=F, cluster_cols=F,fontsize = 7,
+                         cluster_rows=F, cluster_cols=F,fontsize = 4,
                          annotation_names_col = TRUE, annotation_legend = FALSE, 
                          show_colnames = F)
 
-## ---- fig.show='hold', fig.width=3.2, fig.height=3.5---------------------
+## ----corr_samples_distrib, fig.show='hold', fig.width=3.2, fig.height=3.5----
 sample_cor_norm <- plot_sample_corr_distribution(quantile_normalized_matrix,
                                                  example_sample_annotation, 
                                                  batch_col = 'MS_batch', 
@@ -252,7 +252,7 @@ sample_cor_batchCor <- plot_sample_corr_distribution(batch_corrected_matrix,
                                                      plot_param = 'batch_replicate')
 sample_cor_batchCor + theme(axis.text.x = element_text(angle = 45, hjust = 1)) + ylim(0.7, 1)
 
-## ---- fig.show='hold',fig.width=3, fig.height=2.5------------------------
+## ----correlation_of_peptides, fig.show='hold',fig.width=3, fig.height=2.5----
 peptide_cor_norm <- plot_peptide_corr_distribution(quantile_normalized_matrix, 
           generated_peptide_annotation, protein_col = 'Gene', plot_title = 'Quantile normalized')
 peptide_cor_norm + geom_hline(yintercept=0, linetype="dashed", color = "grey")
@@ -260,4 +260,7 @@ peptide_cor_norm + geom_hline(yintercept=0, linetype="dashed", color = "grey")
 peptide_cor_batchCor <- plot_peptide_corr_distribution(batch_corrected_matrix, 
           generated_peptide_annotation, protein_col = 'Gene', plot_title = 'Batch corrected')
 peptide_cor_batchCor + geom_hline(yintercept=0, linetype="dashed", color = "grey")
+
+## ----citation------------------------------------------------------------
+citation('proBatch')
 
