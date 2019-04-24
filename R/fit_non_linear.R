b015e42bc21c16a319c2dd2529e267813d4ee44a
Jelena Chuklina <chuklina.jelena@gmail.com>
2019-04-24 15:51:52 +0200

    fixing non-linear fit to make it more generalizable (stub for the case of other curve fitting other than LOESS); fixed errors for LOESS optimization option; fixed fit for the case with m_scores
    Former-commit-id: c122bc9fdf0b5d603e36f72c274df90124d75cc5 [formerly 840e6838e82bd51e2784ddd45354ec48a5e8246f [formerly 94dbd3b2ccebbb87d7e2c77ea853f325fec1bc57] [formerly 281f32a3b101514e12abbe2fd812b5d85654911a]]
    Former-commit-id: b5a56240d9d921e71f8cf5fd1d51aa7cfbcb2bdd [formerly 1f8dec3b9c049a235240bd4fe727bfae27651446] [formerly ca5a9b79758c93dcea91a5e969f1dcd6b418ec8e]
    Former-commit-id: 7912bf88bbe7f9f89371e931c3854794086de52b
    Former-commit-id: defb56b7df5eaba511bd7bb61d91301f5ebb86fc

--- R/fit_non_linear.R
+++ R/fit_non_linear.R
@@ -1,20 +1,20 @@
 
-#' Fit a non-linear trend
+#' Fit a non-linear trend (currently optimized for LOESS)
 #'
-#' @param df_long data frame containing response variable e.g. 
+#' @param df_peptide data frame containing response variable e.g. 
 #' samples in order and explanatory 
 #'   variable e.g. measurement
 #' @param batch.size the total number of samples in the batch to 
 #' compute for percentage threshold 
-#' @param response.var the name of the column in dataDF with 
-#' the response variable
-#' @param expl.var the name of the column in \code{df_long} with the 
-#' explanatory variable
+#' @param response.var the name of the column in \code{df_peptide} (or, generally, in \code{df_long}) with 
+#' the response variable, usually \code{measure_col}
+#' @param expl.var the name of the column in \code{df_peptide} with the 
+#' explanatory variable, in most cases order
 #' @param noFitRequants (logical) whether to fit requanted values
 #' @param fitFunc function to use for the fit (\code[loess_regression})
-#' @param with_df logical, whether to specify span by enp.target 
-#' using approximately equivalent 
-#'   number of parameters
+#' @param optimize_span logical, whether to specify span or optimize it
+#' @param qual_col column, indicating whether the "quality" variable is specified
+#' @param qual_value 
 #' @param abs_threshold the absolute threshold to filter 
 #' data for curve fitting 
 #' @param pct_threshold the percentage threshold to filter 
@@ -25,52 +25,112 @@
 #' 
 #' @keywords internal
 #' 
-fit_nonlinear <- function(df_long, batch.size, response.var = 'y', expl.var = 'x',
-                          noFitRequants = FALSE, fitFunc = 'loess_regression',
-                          with_df = FALSE, 
+fit_nonlinear <- function(df_peptide, batch.size = NULL, response.var = 'y', expl.var = 'x',
+                          feature.id = NULL, batch.id = NULL,
+                          fitFunc = 'loess_regression',
+                          optimize_span = FALSE, 
+                          noFitRequants = FALSE, qual_col = 'm_score', qual_value = 2,
                           abs_threshold = 5, pct_threshold = 0.20, ...){
 
-    df_long <- df_long[sort.list(df_long[[expl.var]]),]
-    x_to_fit = df_long[[expl.var]]
-    y = df_long[[response.var]]
-    pct_threshold = batch.size*pct_threshold
-    if(fitFunc == "loess_regression"){
-        if(length(x_to_fit) >= abs_threshold & length(x_to_fit) >= pct_threshold){
-            x_all = x_to_fit
-            if(noFitRequants){
-                x_all[df_long$requant] = NA
-            }
-            if(!with_df){
-                fit_res = switch(fitFunc,
-                                 loess_regression = loess_regression(
-                                   x_all, y, x_to_fit,...)
-                                 )
-            } else {
-                bw = optimise_bw(df_long, response.var = response.var, 
-                                 expl.var = expl.var)
-                df = optimise_df(df_long, bw, response.var = response.var, 
-                                 expl.var = expl.var)
-                fit_res = switch(fitFunc,
-                                 loess_regression = loess_regression_opt(
-                                   x_all, y, x_to_fit, df, ...))
-            }
-        }else{
-            fit_res = rep(NA, length(x_to_fit))
-        }
-    }else{
-        stop("Only loess regression fitting is available for current version")
+    df_peptide <- df_peptide[sort.list(df_peptide[[expl.var]]),]
+    x_all = df_peptide[[expl.var]]
+    y = df_peptide[[response.var]]
+    
+    if(noFitRequants){
+      if(!is.null(qual_col) && (qual_col %in% names(df_peptide))){
+        warning('imputed value column is in the data, fitting curve only to measured, non-imputed values')
+        imputed_values <- df_peptide[[qual_col]] == qual_value
+        x_to_fit = x_all[!imputed_values]
+        y = y[!imputed_values]
+      } else {
+        stop('imputed values are specified not to be used for curve fitting, however, no flag for imputed values is specified')
+      }
+      
+    } else {
+      if(!is.null(qual_col) && (qual_col %in% names(df_peptide))){
+        warning('requant column is in the data, are you sure you want to fit non-linear curve to these values, too?')
+      }
+      x_to_fit = x_all
+    } 
+    
+    #checking if there is a reasonable number of values to fit any sensible curve
+    if (is.null(batch.size)){
+      warning("Batch size is not specified, assuming number of entries for this 
+              batch and feature is the total batch size. Maybe erroneous,
+              if missing values are not NAs, but removed from data frame completely")
+      batch.size = nrow(df_peptide)
     }
-    return(fit_res)
+    
+    pct_threshold = batch.size*pct_threshold
+    if(length(x_to_fit) >= abs_threshold & length(x_to_fit) >= pct_threshold){
+      #fitting the curve
+      #TODO: re-write in the functional programming paradigm (e.g. arguments - function, x_all, y, x_to_fit)
+      if(fitFunc == 'loess_regression'){
+        if(!optimize_span){
+          fit_res = loess_regression(x_all, y, x_to_fit,...)
+        } else {
+          fit_res = loess_regression_opt(x_to_fit, y, x_all, ...)
+        }
+      } else{
+        stop("Only loess regression fitting is available for current version")
+      }
+    }else{
+      warning(sprintf("Curve fitting didn't have enough points to fit for the feature %s
+                      in the batch %s, leaving the original value", feature.id, batch.id))
+      fit_res = y
+    }
+  return(fit_res)
 }
 
 loess_regression <- function(x_all, y, x_to_fit, ...){
     fit = loess(y ~ x_to_fit, surface = 'direct', ...)
-    pred <- predict(fit, newdata = x_to_fit)
+    pred <- predict(fit, newdata = x_all)
     return(pred)
 }
 
-loess_regression_opt <- function(x_all, y, x_to_fit, df, ...){
-    fit = loess(y ~ x_all, enp.target = df, surface = 'direct', ...)
-    res = predict(fit, newdata = x_to_fit)
-    return(res)
+loess_regression_opt <- function(x_to_fit, y, x_all, ...) {
+  bw = optimise_bw(x_to_fit, y, ...)
+  degr_freedom = optimise_df(x_to_fit, bw)
+  fit = loess(y ~ x_all, enp.target = degr_freedom, surface = 'direct', ...)
+  res = predict(fit, newdata = x_to_fit)
+  return(res)
 }
+
+loocv.nw <- function(x, y, bw = 1.5, kernel = "normal"){
+  ## Help function to calculate leave-one-out regression values
+  loo.reg.value.bw <- function(i, x, y, bw, kernel = kernel){
+    return(ksmooth(x[-i], y[-i],
+                   x.points = x[i],
+                   kernel = kernel, bandwidth = bw)$y)
+  }
+    
+    
+  ## Calculate LOO regression values using the help function above
+  
+  n <- max(length(x), length(y))
+  loo.values.bw <- sapply(1:n, loo.reg.value.bw, x, y, bw, kernel)
+  ## Calculate and return MSE
+  return(mean((y - loo.values.bw)^2))
+}
+
+optimise_bw <- function(x, y, kernel = 'normal',
+                        bws = c(0.01, 0.5, 1, 1.5, 2, 5, 10)){
+  cv.nw_mult = sapply(bws, function(bw) loocv.nw(x, y, bw, kernel))
+  bw_best = bws[which.min(cv.nw_mult)]
+  return(bw_best)
+}
+
+reg.fcn.nw <- function(reg.x, reg.y, x, bw = 1.5)
+  ksmooth(reg.x, reg.y, x.points = x, kernel = "normal", bandwidth = bw)$y
+
+optimise_df <- function(x, bw){
+  #find the best bandwidth
+  
+  n <- length(x)
+  Id <- diag(n)
+  S.nw <- matrix(0, n, n)
+  for (j in 1:n)
+      S.nw[, j] <- reg.fcn.nw(x, Id[,j],x, bw = bw)
+    df.nw <- sum(diag(S.nw))
+    return(df.nw)
+  }
