9aac599358aa4a10837bb76d9ab592d3028e52aa
Jelena Chuklina <chuklina.jelena@gmail.com>
2019-05-09 15:40:02 +0200

    corrected "annotation_col" to "protein_col" in peptide_annotation creating script
    Former-commit-id: d438f364573f27d1f75b41ac466b5bd7171b9dec [formerly 3a828dd9570e3e35f09a4d4d86b8e4606ddc9b49 [formerly 4720432afb003b8f53dd3d52804af493e48a0f49] [formerly f5138ef774460b5d3614f9ee4dce76537fd31ef0 [formerly 3592b80aed74bb424218880865c98274564cd83d]] [formerly 59cb228f5950f91c6f6f1c88f63ff1fed9c9ae80] [formerly 5381ff493fe1116df87c6a0c031ad3af84c75e4f]]
    Former-commit-id: fbd3fa714f8fab6ba22a6afab33a07dfcf0d0cd4 [formerly 17742be87fa9361f3c3d74451d8418e2e500d86b] [formerly 152192c6c4af73ef690c0f78595a79c6fcaaff6e [formerly 6c96722bd8a69b959cff46f9c631d7dd902e92e2]] [formerly 7faf9c827c1cc93db5d22d3d9a1a64d86ddc22d0] [formerly f29f298265c0748a38127a2e5b383f06c1df7a42]
    Former-commit-id: 4574d784ff7a972d00a3513b4f2cdf2e3b702db1
    Former-commit-id: 0bba8feb60a748c37226f00ee6d183549393f229
    Former-commit-id: 894f77d805f7a2c3a13b066f16e8b573524b0a54 [formerly 905797c0bfc550a2ff1d32146f8721640f22a11f]
    Former-commit-id: c884065ebdb6bd83ba047a019110105868f4e64c

--- R/auxiliary.R
+++ R/auxiliary.R
@@ -82,22 +82,20 @@ matrix_to_long <- function(data_matrix, sample_annotation = NULL,
 #' for selection of illustrative proteins
 #'
 #' @inheritParams proBatch
-#' 
-#' @param annotation_col one or more columns contatining protein ID
 #'
 #' @return data frame containing petpide annotations 
 #' @export
 #' @examples 
 #' generated_peptide_annotation <- create_peptide_annotation(
 #' example_proteome, feature_id_col = "peptide_group_label",
-#' annotation_col = c("ProteinName" ))
+#' protein_col = c("ProteinName" ))
 #' 
 #' @seealso \code{\link{plot_peptides_of_one_protein}}, 
 #' \code{\link{plot_protein_corrplot}}
 create_peptide_annotation <- function(df_long, feature_id_col = 'peptide_group_label',
-                                      annotation_col = c("ProteinName" )){
+                                      protein_col = c("ProteinName" )){
   peptide_annotation = df_long %>%
-    select(one_of(c(feature_id_col, annotation_col))) %>%
+    select(one_of(c(feature_id_col, protein_col))) %>%
     distinct()
   return(peptide_annotation)
 }
