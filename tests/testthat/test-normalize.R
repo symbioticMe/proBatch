9803d09e3d9e7062510d05263910e26bafaaeb4b
Jelena Chuklina <chuklina.jelena@gmail.com>
2019-05-17 17:34:01 +0200

    corrected the tests for normalization and the initial assessment
    Former-commit-id: 7c6b2543144445530125ad59c3b9ea1cdd5ef7a1
    Former-commit-id: 8745746ca1c262b8a328214c57fdcdffd5fe2cbc
    Former-commit-id: 88585aab9e2408c6f36e25025926678cf35218da

--- tests/testthat/test-normalize.R
+++ tests/testthat/test-normalize.R
@@ -1,24 +1,5 @@
 context("normalize")
 
-
-test_that("log_transformed_matrix", {
-  data(example_proteome_matrix, package="proBatch")
-
-  matrix <- example_proteome_matrix[1:10, ]
-  log2_transformed_matrix <- log_transform(matrix, log_base = 2)
-  log10_transformed_matrix <- log_transform(matrix, log_base = 10)
-  
-  log2_matrix = log2(matrix + 1)
-  log10_matrix = log10(matrix + 1)
-  
-  expect_equivalent(log2_matrix[1:10], log2_transformed_matrix[1:10])
-  expect_equivalent(log10_matrix[1:10], log10_transformed_matrix[1:10])
-  
-  expect_error(log_warn <- log_transform(matrix, log_base = 8))
-  
-})
-
-
 test_that("quantile_normalize", {
   data(example_proteome_matrix, package="proBatch")
   
@@ -39,13 +20,13 @@ test_that("normalize_sample_medians", {
              36458, 16956, 83006,  6865, 35359, 34456, 53969, 16449, 34586, 69234, 61117, 25524, 
              54715, 25445, 13009, 71210, 11979, 75665, 48261, 34331, 44354, 31732,  6913, 81975, 
              19950, 14439, 53985, 44994, 8980, 54782, 46463, 67231, 66593, 23441, 50172, 24628, 72037, 49198)
-  sample_data <- example_proteome[sampled_rows, ]
-  median_centered_data <- normalize_sample_medians(sample_data)
+  sample_df <- example_proteome[sampled_rows, ]
+  median_centered_data <- normalize_sample_medians(sample_df)
 
   expect_equivalent(median_centered_data$median_global[1], 32298.40)
   expect_equivalent(median_centered_data$median_global[2], 32298.40)
   
-  expect_equivalent(median_centered_data$Intensity_normalized[1], 32298.4)
-  expect_equivalent(median_centered_data$Intensity_normalized[2], 32298.4)
+  expect_equivalent(median_centered_data$Intensity[1], 32298.4)
+  expect_equivalent(median_centered_data$Intensity[2], 32298.4)
   
 })
