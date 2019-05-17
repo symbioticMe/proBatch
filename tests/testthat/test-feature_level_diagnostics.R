b960a2e0e2507c880ed9ad4aeacb66ed75ee1205
Jelena Chuklina <chuklina.jelena@gmail.com>
2019-05-17 15:40:35 +0200

    corrected bugs in function calling of feature diagnostics
    Former-commit-id: f1b57ce60d1fe09eb72422c52548d2ee2cb1f842 [formerly 0759536512d98aa6f48f2eeebe14100bb852bc96 [formerly fb49c3d136bf5a87fcc9227bc04f4c111f6eab61] [formerly 2457d23de6c9ab81849448fd6bc2a7ccc8f1c5ee [formerly 6ad1c3a6e4036242370e9067e305abe179f82ffb]] [formerly f0903dd0738d4a8f4b50a64974200d55a48eedd8] [formerly 2a16bc39c8571deab032c3607a438281a587d0e2]]
    Former-commit-id: ade1583778e5bc5704f28ec50ead3cc214cf8848 [formerly 3162536bae8e0f6670c9ce87f9635e41e4230e8d] [formerly cd24f543313caa4859b7929a1bf967570c4d1b33 [formerly f07af17e986dd24c52b0833cb15dc41c166da7f9]] [formerly 9ef9c8fa5f50c84888215766a8a20dce90c5d60e] [formerly 6986ba61b56513b1320be3c8d4192ec236e62282]
    Former-commit-id: 48ee8135cb57e61d388a6bb7ced49ed12e358952
    Former-commit-id: 39ba179785fa77b259e0ff79ea336bfd849bc46c
    Former-commit-id: 9fb6b515c0ab41b4994aa4686d6f8a0e5c574d87 [formerly 233ec9b4c47d89776801b05198f7347f223c77a9]
    Former-commit-id: 6357d7142ce4a026fcb7b77fd94ff5df39aa411c

--- tests/testthat/test-feature_level_diagnostics.R
+++ tests/testthat/test-feature_level_diagnostics.R
@@ -5,10 +5,11 @@ test_that("single_feature_plot", {
   data(example_proteome, package="proBatch")
   data(example_sample_annotation, package="proBatch")
   
-  single_feature <- plot_single_feature(pep_name = "46213_NVGVSFYADKPEVTQEQK_2", 
-                      df_long = example_proteome, example_sample_annotation, color_by_col = NULL)
+  single_feature <- plot_single_feature(feature_name = "46213_NVGVSFYADKPEVTQEQK_2", 
+                                        df_long = example_proteome, 
+                                        sample_annotation = example_sample_annotation)
 
-  expect_equal(single_feature$plot_env$pep_name,  "46213_NVGVSFYADKPEVTQEQK_2")
+  expect_equal(single_feature$plot_env$feature_name,  "46213_NVGVSFYADKPEVTQEQK_2")
   
   expect_equal(single_feature$labels$x, "order")
   expect_equal(single_feature$labels$y, "Intensity")
@@ -28,12 +29,13 @@ test_that("peptides_of_one_protein_plot", {
   
   peptides_plot <- plot_peptides_of_one_protein (protein_name = "Haao",  
                                 protein_col = "Gene", df_long = example_proteome, 
-                                example_sample_annotation, color_by_batch = TRUE,
+                                sample_annotation = example_sample_annotation, 
+                                color_by_batch = TRUE,
                                 order_col = 'order', sample_id_col = 'FullRunName', 
                                 batch_col = 'MS_batch')  
   
-  expect_equal(peptides_plot$plot_env$pep_name[1],  "10231_QDVDVWLWQQEGSSK_2")
-  expect_equal(peptides_plot$plot_env$pep_name[2],  "10768_RLESELDGLR_2" )
+  expect_equal(peptides_plot$plot_env$feature_name[1],  "10231_QDVDVWLWQQEGSSK_2")
+  expect_equal(peptides_plot$plot_env$feature_name[2],  "10768_RLESELDGLR_2" )
 
   expect_equal(peptides_plot$labels$x, "order")
   expect_equal(peptides_plot$labels$y, "Intensity")
@@ -50,12 +52,13 @@ test_that("spike_in_peptides_plot", {
   data(example_proteome, package="proBatch")
   data(example_sample_annotation, package="proBatch")
   
-  spike_in <- plot_spike_in(example_proteome, example_sample_annotation, 
-                protein_col = 'Gene', spike_ins = "BOVINE_A1ag", 
-                plot_title = "Spike-in BOVINE protein peptides")
+  spike_in <- plot_spike_in(spike_ins = "BOVINE_A1ag", 
+                            protein_col = 'Gene',  df_long = example_proteome,
+                            sample_annotation = example_sample_annotation,
+                            plot_title = "Spike-in BOVINE protein peptides")
   
-  expect_equal(spike_in$plot_env$pep_name[1],  "10062_NVGVSFYADKPEVTQEQK_3")
-  expect_equal(spike_in$plot_env$pep_name[2],  "10063_NVGVSFYADKPEVTQEQKK_3")
+  expect_equal(spike_in$plot_env$feature_name[1],  "10062_NVGVSFYADKPEVTQEQK_3")
+  expect_equal(spike_in$plot_env$feature_name[2],  "10063_NVGVSFYADKPEVTQEQKK_3")
   
   expect_equal(spike_in$labels$x, "order")
   expect_equal(spike_in$labels$y, "Intensity")
@@ -75,11 +78,12 @@ test_that("iRT_peptides_plot", {
   data(example_proteome, package="proBatch")
   data(example_sample_annotation, package="proBatch")
   
-  iRT <- plot_iRT(example_proteome, example_sample_annotation, 
-                            protein_col = 'Gene', irt_pattern = 'iRT')
+  iRT <- plot_iRT(irt_pattern = 'iRT', protein_col = 'Gene',
+                  df_long = example_proteome,
+                  sample_annotation = example_sample_annotation)
   
-  expect_equal(iRT$plot_env$pep_name[1],   "1146_ADVTPADFSEWSK_3")
-  expect_equal(iRT$plot_env$pep_name[2], "12476_TPVISGGPYEYR_2")
+  expect_equal(iRT$plot_env$feature_name[1],   "1146_ADVTPADFSEWSK_3")
+  expect_equal(iRT$plot_env$feature_name[2], "12476_TPVISGGPYEYR_2")
   
   expect_equal(iRT$labels$x, "order")
   expect_equal(iRT$labels$y, "Intensity")
@@ -98,14 +102,16 @@ test_that("fitting_trend_plots", {
   data(example_proteome_matrix, package="proBatch")
   data(example_sample_annotation, package="proBatch")
   
-  matrix <- example_proteome_matrix[1:10, ]
-  loess_fit <- adjust_batch_trend(matrix, example_sample_annotation, span = 0.7)
+  short_df <- example_proteome[example_proteome$peptide_group_label %in% 
+                                 unique(example_proteome$peptide_group_label)[1:3], ]
+  loess_fit <- adjust_batch_trend(short_df, example_sample_annotation, span = 0.7)
   
-  fit_plot <- plot_with_fitting_curve(pep_name = "10062_NVGVSFYADKPEVTQEQK_3", 
-                          df_long = example_proteome, example_sample_annotation, 
-                          fit_df = loess_fit$fit_df)
+  fit_plot <- plot_with_fitting_curve(feature_name = "10062_NVGVSFYADKPEVTQEQK_3", 
+                                      fit_df = loess_fit$fit_df, fit_value_var = 'fit',
+                                      df_long = example_proteome, 
+                                      sample_annotation = example_sample_annotation)
   
-  expect_equal(fit_plot$plot_env$pep_name[1],   "10062_NVGVSFYADKPEVTQEQK_3")
+  expect_equal(fit_plot$plot_env$feature_name[1],   "10062_NVGVSFYADKPEVTQEQK_3")
 
   expect_equal(fit_plot$labels$x, "order")
   expect_equal(fit_plot$labels$y, "Intensity")
