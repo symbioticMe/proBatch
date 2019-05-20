80d9ca2498e2a14873769d2c1563e4361d5a3387
Jelena Chuklina <chuklina.jelena@gmail.com>
2019-05-20 17:40:28 +0200

    updated date conversion tests
    Former-commit-id: 8a598fb10365041fe727572d0dde3018bf650aaa [formerly 5799a46ca9da5edd6e3a0ed6a5f257735c8cdeb1 [formerly 3e75affaabb559a37b8ed7829ff56ef3c6877388] [formerly 6a60a68a05347e956ae1e6b1de12a0f93f49ba4f [formerly b9d8efc435d88275315f88acee923567c3a66a9e]] [formerly 94bff42f9e327b90d7e518cdc63c1b93d4fa5dd6] [formerly 4e5676bb8ad61e37cb8a0035556d4e2fc23233aa]]
    Former-commit-id: d9717f96270463bab3553063abcc599de32cdb3e
    Former-commit-id: b435b418178190f369adf2ea92c9f5ac49ec5800
    Former-commit-id: b23de8adc307609f30a60308d0f7b86f61d31264
    Former-commit-id: 3517075403c4a2365fcb140c386b3d213bc03e1b [formerly 499514230f477c6c679b112b7d5b28121246d6b3]
    Former-commit-id: 16c4d1a39d63b595df30fdcba4de8b17b8feeb6e

--- tests/testthat/test-date_conversion.R
+++ tests/testthat/test-date_conversion.R
@@ -12,30 +12,39 @@ test_that("dates_to_posix", {
                                    new_time_column = 'DateTime', 
                                    dateTimeFormat = c("%b_%d", "%H:%M:%S"))
   
-  expect_equal(droplevels(new_annotation$RunDate[1]), as.factor("Oct_05"))
-  expect_equal(droplevels(new_annotation$RunTime[1]), as.factor("18:35:00"))
+  expect_equal(new_annotation$RunDate[1], "Oct_05")
+  expect_equal(as.character(new_annotation$RunTime[1]), "18:35:00")
   expect_equal(new_annotation$DateTime[1], as.POSIXlt("2019-10-05 18:35:00 CEST"))
-
+  
+  expect_is(new_annotation$DateTime, "POSIXct")
+  expect_is(new_annotation$RunTime, "difftime")
 })
 
 
 test_that("date_to_sample_order", {
   data(example_sample_annotation, package="proBatch")
   
-  sample_annotation <- example_sample_annotation[1:5,]
-  sample_annotation$DateTime <- NULL
-  sample_annotation$order <- NULL
+  sample_annotation_test <- example_sample_annotation[1:5,]
+  sample_annotation_test$DateTime <- NULL
+  sample_annotation_test$order <- NULL
   
-  new_annotation_worder <- date_to_sample_order(example_sample_annotation,
+  new_annotation_worder <- date_to_sample_order(sample_annotation_test,
                                           time_column = c('RunDate','RunTime'),
                                           new_time_column = 'new_DateTime',
                                           dateTimeFormat = c("%b_%d", "%H:%M:%S"),
                                           new_order_col = 'new_order',
                                           instrument_col = NULL)
   
-  expect_equal(droplevels(new_annotation_worder$RunDate[1]), as.factor("Oct_05"))
-  expect_equal(droplevels(new_annotation_worder$RunTime[1]), as.factor("18:35:00"))
+  expect_equal(new_annotation_worder$RunDate[1], "Oct_05")
+  expect_equal(as.character(new_annotation_worder$RunTime[1]), "18:35:00")
+  
+  expect_is(new_annotation_worder$new_DateTime, "POSIXct")
+  expect_is(new_annotation_worder$RunTime, "difftime")
+  expect_length(new_annotation_worder$new_order, nrow(sample_annotation_test))
+  
   expect_equal(new_annotation_worder$new_order[1], 1)
   expect_equal(new_annotation_worder$new_order[2], 2)
+  
+  
 
 })
