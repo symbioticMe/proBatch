ee8b32ae2f3f0666de73a63095e566b7c0798181
Jelena Chuklina <chuklina.jelena@gmail.com>
2019-05-20 17:41:01 +0200

    added utility function tests
    Former-commit-id: fdfaf8c50cd2268b73843d807f5aae3e05229be3 [formerly d4475f8d6b70d8d57143bc79c5965f0aa74f9fc7 [formerly e43c8956ec0abdffcbee1d8235c4ccb4e11828fa] [formerly a6478c3371b07652667ba97066da494559ece5b2] [formerly 942287845d20258d3bfe2f642ed4f69691d6beef]]
    Former-commit-id: b2bba8770c94f8730a13cc7eaa29a7c982e134ff [formerly 0760c97d3823cd0e56c6c8fb86114b7662b7075b] [formerly ecfca74893266b8d7f40486571b5164c0bc3a4fd] [formerly 9d9e7a3d80c26c14a60940a276499cac89152788]
    Former-commit-id: 7142df397a06229a4d05b6956c7c5ea7adcd038c
    Former-commit-id: 7586cff941b57703994a9f1eca4c0d2c09e818a4
    Former-commit-id: b5d18bd61e24484166894ddb36a46932f723a0e4

---
+++ tests/testthat/test-utility_funcs.R
@@ -0,0 +1,60 @@
+context("utility_funcs")
+
+test_that("check_sample_consistency", {
+  #TODO: check if the warnings are raised expect_warning
+  
+  df_test = check_sample_consistency(sample_annotation = example_sample_annotation, 
+                                     df_long = example_proteome, 
+                                     sample_id_col = 'FullRunName',
+                                     batch_col = NULL, order_col = NULL, facet_col = NULL)
+  
+  expect_equal(ncol(df_test), ncol(example_sample_annotation) + ncol(example_proteome) -1)
+  expect_equal(nrow(df_test), nrow(example_proteome))
+  
+  options(warn = 0)
+  expect_warning(df_test = check_sample_consistency(sample_annotation = NULL, 
+                                                    df_long = example_proteome, 
+                                                    sample_id_col = 'FullRunName',
+                                                    batch_col = NULL, order_col = NULL, facet_col = NULL))
+  
+  falsified_annotation = example_sample_annotation
+  colnames(falsified_annotation)[colnames(falsified_annotation) == 'FullRunName'] = 'FalseRunName'
+  expect_error(df_test = check_sample_consistency(sample_annotation = falsified_annotation, 
+                                                    df_long = example_proteome, 
+                                                    sample_id_col = 'FullRunName',
+                                                    batch_col = NULL, order_col = NULL, facet_col = NULL))
+})
+
+test_that("define_sample_order", {
+  #TODO: check if the returned class is integer expect_is
+  
+  order_col <- 'order'
+  sample_order = define_sample_order(order_col = order_col, 
+                                     sample_annotation = example_sample_annotation, 
+                                     facet_col = NULL, batch_col = 'MS_batch', 
+                                     df_long = example_proteome, 
+                                     sample_id_col = 'FullRunName', 
+                                     color_by_batch = T)
+  new_order_col = sample_order$order_col
+  df_long = sample_order$df_long
+  
+  expect_is(sample_order, 'list')
+  expect_equal(df_long$order_col, example_sample_annotation$order_col)
+  
+  expect_is(df_long[[new_order_col]], 'numeric')
+  
+  order_col = NULL
+  sample_order = define_sample_order(order_col = order_col, 
+                                     sample_annotation = example_sample_annotation, 
+                                     facet_col = NULL, batch_col = 'MS_batch', 
+                                     df_long = example_proteome, 
+                                     sample_id_col = 'FullRunName', 
+                                     color_by_batch = T)
+  new_order_col = sample_order$order_col
+  df_long = sample_order$df_long
+  
+  expect_is(sample_order, 'list')
+  expect_equal(df_long$order_col, example_sample_annotation$order_col)
+  
+  expect_is(df_long[[new_order_col]], 'factor')
+})
