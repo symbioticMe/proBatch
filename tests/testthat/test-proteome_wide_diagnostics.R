45747897ee9da7b6e9e0b2083287602008f30e65
Jelena Chuklina <chuklina.jelena@gmail.com>
2019-05-20 17:40:47 +0200

    updated proteom_wide_diagnostics test
    Former-commit-id: cb665721944ec83645cd804815757c87563f915a [formerly c673f2a01085799acde74834e1f1a1549abd1db6] [formerly 40974fcb56aae9717eb20968f5d43c20c55fd55d] [formerly 4c362b2b11ab20a6a240ae58e0f768f82662d8ea] [formerly 09e84ef8befaa425ed082769de708e1b149bb31b [formerly c85ab7ed51e5453eb5701e99c41aa195c521d403] [formerly bfdad673b03a197397356efd2876f64e847ddce2] [formerly 4d7f475b9c15b7aeb5019b89163fe340cf79f7b3]] [formerly 9a504985f82372f77a65b1a0fc316bd68efc40ed [formerly 2b6030c3994789abd47a1cc3b8a0e80f422a3a53 [formerly 9e73d5757ea25d89ab9693d24752a19a64227f6a] [formerly 6e523be02b7cd163ee904c14b080498a96ce75a9] [formerly 99c5817a718af38c66c6d37956f2920fb543f5ce]]]
    Former-commit-id: 40a4c74a53163ecb513c5162e58e4d808bae12fb [formerly 9a8af00ca937621e93b77fe0b4d9a3e9a9895b5f [formerly 9c96eda5c373540f0ac1b0982afb98cc3950b906] [formerly f44f6e26f957c2218a1f4e14257328c77b770f9e] [formerly a1615e96b68a7623153cb0b6c224e640b6cdb7c9]]
    Former-commit-id: 9c94e75fe2a23824bf8fe65420cf2ff8a214b823
    Former-commit-id: a0a09090f831b97985772a283e8bc42a47b63c33
    Former-commit-id: 089b61573630299148ce19e36e524f6cc0196a2d
    Former-commit-id: 8403303700a44e973109c4a5067a696f8aebdbdb

--- tests/testthat/test-proteome_wide_diagnostics.R
+++ tests/testthat/test-proteome_wide_diagnostics.R
@@ -5,10 +5,10 @@ test_that("pca_heatmap", {
   data(example_proteome_matrix, package="proBatch")
   data(sample_color_scheme, package="proBatch")
   
-  matrix <- example_proteome_matrix[1:10, ]
+  matrix_test <- example_proteome_matrix[1:10, ]
   color_annotation <- sample_color_scheme$color_df[,c("MS_batch", "Diet")]
  
-  hiearchical <- plot_hierarchical_clustering(matrix, color_annotation,  
+  hiearchical <- plot_hierarchical_clustering(matrix_test, color_annotation,  
                                distance = "euclidean", agglomeration = 'complete',
                                label_samples = FALSE)
   
@@ -25,9 +25,9 @@ test_that("heatmap_plot", {
   data(example_sample_annotation, package="proBatch")
   data(sample_color_scheme, package="proBatch")
   
-  matrix <- example_proteome_matrix[1:20, ]
+  matrix_test <- example_proteome_matrix[1:20, ]
   
-  expect_warning(heatmap <- plot_heatmap(matrix, example_sample_annotation, 
+  expect_warning(heatmap <- plot_heatmap(matrix_test, sample_annotation = example_sample_annotation, 
                       sample_annotation_col = c("MS_batch",  "digestion_batch", "Diet"), 
                       cluster_cols = TRUE, annotation_color_list = sample_color_scheme$list_of_colors,
                       show_rownames = TRUE, show_colnames = FALSE))
@@ -52,14 +52,14 @@ test_that("pvca_plot", {
   data(example_proteome_matrix, package="proBatch")
   data(example_sample_annotation, package="proBatch")
   
-  matrix <- example_proteome_matrix[1:50, ]
-  pvca <- plot_PVCA(matrix, example_sample_annotation, 
+  matrix_test <- example_proteome_matrix[1:50, ]
+  pvca <- plot_PVCA(matrix_test, example_sample_annotation, 
                    technical_covariates = c('MS_batch', 'digestion_batch'),
                    biological_covariates = c("Diet", "Sex", "Strain"))
   
   expect_equivalent(pvca$plot$data$label[1], factor("Strain"))
   expect_equivalent(pvca$plot$data$label[2], factor("digestion_batch"))
-  expect_equivalent(pvca$plot$data$label[3], factor("Sex:Strain"))
+  expect_equivalent(pvca$plot$data$label[3], factor("digestion_batch:Strain"))
   expect_equivalent(pvca$plot$data$label[4], factor("MS_batch"))
   
   expect_equal(pvca$plot$data$category[1], "biological")
@@ -74,7 +74,7 @@ test_that("pca_plot", {
 
   pca <- plot_PCA(example_proteome_matrix, example_sample_annotation, 
                         color_by = 'MS_batch', plot_title = "PCA colored by MS batch")
-  expect_equal(pca$labels$y, "PC2 (14.33%)")
-  expect_equal(pca$labels$x, "PC1 (69.74%)")
+  expect_equal(pca$labels$y, "PC2 (14.23%)")
+  expect_equal(pca$labels$x, "PC1 (69.41%)")
   expect_equal(pca$labels$colour, "MS_batch")
 })
