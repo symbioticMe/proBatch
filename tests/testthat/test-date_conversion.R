75a3e926912f92c2465847a9d2fb1329dc6b16e9
Jelena Chuklina <chuklina.jelena@gmail.com>
2019-05-21 16:23:05 +0200

    fixed outdated test parts in 'date_to_posix' and 'date_to_sample_order'
    Former-commit-id: fdfaa58349e557b6e3958c1cfd6a67e695322c3a [formerly d47da747cee98b1dc754ca4e62e12ea088dbd175] [formerly 5ecd51bc7d0c234f7c4321c50467a64360a0c3b3] [formerly 6e4b8cef927d612606a40edc563f07ec67da51d2 [formerly 7f46c08939e70989b228a916ed9860bb065445cb] [formerly 19c328676c0ed6a1a8e5d53615f1435987b7975f]] [formerly 0ad346d0bfba03d207a7994c5eec4c7eed2355fe [formerly 345805dd4eab58bc94cf6cea2113cb38b97ca230 [formerly 9087ad779027cd87c8d90ed2c17706d009ec6d60] [formerly b41db928936ac5d1f73e625d9e3436c46a116aec]]]
    Former-commit-id: 736c0cf48118c514531bd5baed046a2d15ff5b81 [formerly 78939ca5bd2c75655fd6a7fdb1b75be2c220f4b0 [formerly bf0cf7eb477127f8b2956bf1f55f623f9c2dd8db] [formerly 679ae63672b7b8a6b7535b7461de89782708958d]]
    Former-commit-id: fab4c0102b34b6f10accc7151697d9039a249193
    Former-commit-id: 90be503081f7efde74ff64f219615b7b4f359f40
    Former-commit-id: d2c66db98aa14064436de9159ae377f629f7941c

--- tests/testthat/test-date_conversion.R
+++ tests/testthat/test-date_conversion.R
@@ -10,14 +10,15 @@ test_that("dates_to_posix", {
   new_annotation <- dates_to_posix(sample_annotation, 
                                    time_column = c('RunDate','RunTime'),
                                    new_time_column = 'DateTime', 
-                                   dateTimeFormat = c("%b_%d", "%H:%M:%S"))
+                                   dateTimeFormat = c("%b_%d", "%H:%M:%S"), 
+                                   tz = 'GMT')
   
   expect_equal(new_annotation$RunDate[1], "Oct_05")
-  expect_equal(as.character(new_annotation$RunTime[1]), "18:35:00")
-  expect_equal(new_annotation$DateTime[1], as.POSIXlt("2019-10-05 18:35:00 CEST"))
+  expect_equal(new_annotation$RunTime[1], "18:35:00")
+  expect_equal(format(new_annotation$DateTime[1], '%d'), '05')
+  expect_equal(format(new_annotation$DateTime[1], '%H'), '18')
   
   expect_is(new_annotation$DateTime, "POSIXct")
-  expect_is(new_annotation$RunTime, "difftime")
 })
 
 
@@ -36,10 +37,9 @@ test_that("date_to_sample_order", {
                                           instrument_col = NULL)
   
   expect_equal(new_annotation_worder$RunDate[1], "Oct_05")
-  expect_equal(as.character(new_annotation_worder$RunTime[1]), "18:35:00")
+  expect_equal(new_annotation$RunTime[1], "18:35:00")
   
   expect_is(new_annotation_worder$new_DateTime, "POSIXct")
-  expect_is(new_annotation_worder$RunTime, "difftime")
   expect_length(new_annotation_worder$new_order, nrow(sample_annotation_test))
   
   expect_equal(new_annotation_worder$new_order[1], 1)
