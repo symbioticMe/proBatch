08b19d54a5897add113c1152c4c0cf6b42e259c0
Jelena Chuklina <chuklina.jelena@gmail.com>
2019-08-07 14:01:15 +0200

    corrected tests for colors
    Former-commit-id: 81f7cf8b437bfaf9c92c60014b04bc66322dcfd3

--- tests/testthat/test-colors_for_annotation.R
+++ tests/testthat/test-colors_for_annotation.R
@@ -10,8 +10,8 @@ test_that("map_factors_to_colors", {
   expect_warning(sample_color_annt <- map_factors_to_colors(sample_annt))
   
   expect_equal(names(sample_color_annt$MS_batch[1]), "Batch_1")
-  expect_equal(sample_color_annt$MS_batch[[1]], "turquoise")
-  expect_equal(sample_color_annt$MS_batch[[2]], "blue")
+  expect_equal(sample_color_annt$MS_batch[[1]], "#7FC97F")
+  expect_equal(sample_color_annt$MS_batch[[2]], "#BEAED4")
 })
 
 
@@ -23,13 +23,9 @@ test_that("map_numbers_to_colors", {
   sample_annt <- example_sample_annotation[,numeric_columns]
   sample_color_annt <- map_numbers_to_colors(sample_annt)
   
-  expect_equal(names(sample_color_annt$color_list), c("DateTime", "order"))
-  expect_equal(sample_color_annt$color_list$DateTime[[1]],"#A6611A")
-  expect_equal(sample_color_annt$color_list$order[[1]], "#D01C8B")
-  
-  expect_equal(names(sample_color_annt$color_list$order[1]), "(0.768,24.2]")
-  expect_equal(names(sample_color_annt$color_list$order[2]), "(24.2,47.4]")
-
+  expect_equal(names(sample_color_annt), c("DateTime", "order"))
+  expect_equal(sample_color_annt$DateTime[[1]],"#A6611A")
+  expect_equal(sample_color_annt$order[[1]], "#D01C8B")
 })
 
 
@@ -38,22 +34,20 @@ test_that("map_numbers_to_colors", {
 test_that("sample_annotation_to_colors", {
   data(example_sample_annotation, package="proBatch")
   
+  factor_columns = c('MS_batch','EarTag', "Strain", 
+                     "Diet", "digestion_batch", "Sex")
+  numeric_columns = c('DateTime', 'order')
   expect_warning(color_scheme <- sample_annotation_to_colors(example_sample_annotation, 
-                                               factor_columns = c('MS_batch','EarTag', "Strain", 
-                                                                  "Diet", "digestion_batch", "Sex"),
-                                               date_columns = 'DateTime',
-                                               numeric_columns = c('order')))
+                                                             factor_columns = factor_columns,
+                                                             numeric_columns= numeric_columns))
   
-  expect_equal(names(color_scheme), c("list_of_colors", "color_df", "sample_annotation"))
-  expect_equal(names(color_scheme$list_of_colors), 
-               c("MS_batch", "EarTag", "Strain", "Diet",  "digestion_batch", "Sex",
-                 "order", "DateTime"))
+  expect_equal(names(color_scheme), c(factor_columns, numeric_columns))
   
-  expect_equal(color_scheme$list_of_colors$MS_batch[[1]], "turquoise")
-  expect_equal(color_scheme$list_of_colors$MS_batch[[2]],  "blue")
+  expect_equal(color_scheme$MS_batch[[1]], "turquoise")
+  expect_equal(color_scheme$MS_batch[[2]], "blue")
   
-  expect_equal(names(color_scheme$list_of_colors$Sex)[1], "F")
-  expect_equal(names(color_scheme$list_of_colors$Sex)[2], "M")
+  expect_equal(color_scheme$DateTime[[1]], "#A6611A")
+  expect_equal(color_scheme$order[[1]], "#D01C8B")
   
 })
 
